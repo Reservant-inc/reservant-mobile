c27370169635f8f69deb74eb9d9e7cfcd0b0c7c3
Aleksander Babij <s25347@pjwstk.edu.pl>
2024-03-07 19:53:02 +0100

    LoginActivity renamed to MainActivity

--- app/src/main/java/com/example/reservant_mobile/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/reservant_mobile/ui/activities/MainActivity.kt
@@ -10,7 +10,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.reservant_mobile.ui.components.InputUserInfo
 
 
-class LoginActivity : ComponentActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
