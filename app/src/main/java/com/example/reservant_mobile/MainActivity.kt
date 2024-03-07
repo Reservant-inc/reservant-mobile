a349594717528808c67c5e8a34416a050e51bdc3
Aleksander Babij <s25347@pjwstk.edu.pl>
2024-03-07 18:03:15 +0100

    remove themes change kotlin version to 1.9.0

--- app/src/main/java/com/example/reservant_mobile/MainActivity.kt
+++ app/src/main/java/com/example/reservant_mobile/MainActivity.kt
@@ -10,20 +10,16 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.reservant_mobile.ui.theme.ReservantmobileTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ReservantmobileTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Text("Android")
-                }
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = MaterialTheme.colorScheme.background
+            ) {
+                Text("Android")
             }
         }
     }
@@ -33,7 +29,5 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    ReservantmobileTheme {
-        Text("Android")
-    }
+    Text("Android")
 }
