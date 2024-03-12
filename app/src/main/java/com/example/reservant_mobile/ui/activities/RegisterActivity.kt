996af5ca74174fb4f7125859e92a817713748462
Michał Wiśniewski <s24845@pjwstk.edu.pl>
2024-03-12 21:35:54 +0100

    Added validation handling in RegisterActivity

--- app/src/main/java/com/example/reservant_mobile/ui/activities/RegisterActivity.kt
+++ app/src/main/java/com/example/reservant_mobile/ui/activities/RegisterActivity.kt
@@ -13,12 +13,18 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.reservant_mobile.ui.components.InputUserInfo
 import com.example.reservant_mobile.ui.components.Logo
 import com.example.reservant_mobile.ui.components.UserButton
+import com.example.reservant_mobile.ui.viewmodels.LoginViewModel
+import com.example.reservant_mobile.ui.viewmodels.RegisterViewModel
 
 @Composable
 fun RegisterActivity() {
+
+    val registerViewModel = viewModel<RegisterViewModel>()
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -29,49 +35,49 @@ fun RegisterActivity() {
         Logo()
 
         InputUserInfo(
-            inputText = "",
-            onValueChange = {},
+            inputText = registerViewModel.firstName,
+            onValueChange = { registerViewModel.firstName = it },
             label = "Name",
             isError = false
         )
         InputUserInfo(
-            inputText = "",
-            onValueChange = {},
+            inputText = registerViewModel.lastName,
+            onValueChange = { registerViewModel.lastName = it },
             label = "Surname",
             isError = false
         )
         InputUserInfo(
-            inputText = "",
-            onValueChange = {},
+            inputText = registerViewModel.birthday,
+            onValueChange = { registerViewModel.birthday = it },
             label = "Birth Date",
             isError = false,
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
         )
         InputUserInfo(
-            inputText = "",
-            onValueChange = {},
+            inputText = registerViewModel.email,
+            onValueChange = { registerViewModel.email = it },
             label = "Email",
             isError = false,
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email)
         )
         InputUserInfo(
-            inputText = "",
-            onValueChange = {},
+            inputText = registerViewModel.phoneNum,
+            onValueChange = { registerViewModel.phoneNum = it },
             label = "Phone number",
             isError = false,
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Phone)
         )
         InputUserInfo(
-            inputText = "",
-            onValueChange = {},
+            inputText = registerViewModel.password,
+            onValueChange = { registerViewModel.password = it },
             label = "Password",
             visualTransformation = PasswordVisualTransformation(),
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
             isError = false,
         )
         InputUserInfo(
-            inputText = "",
-            onValueChange = {},
+            inputText = registerViewModel.confirmPassword,
+            onValueChange = { registerViewModel.confirmPassword = it },
             label = "Repeat Password",
             visualTransformation = PasswordVisualTransformation(),
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
@@ -81,7 +87,7 @@ fun RegisterActivity() {
 
         Spacer(modifier = Modifier.weight(1f))
 
-        UserButton(onClick = { /* Handle Register */ }, label = "Sign up")
+        UserButton(onClick = { println("REGISTER VALIDATION: "+registerViewModel.validateForm()) }, label = "Sign up")
     }
 }
 
