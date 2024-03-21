9fa7a4ec1dda446f3788098f1f7c129b1bdd1a5c
Mroxny <50219282+Mroxny@users.noreply.github.com>
2024-03-21 23:53:02 +0100

    Create Endpoints.kt

---
+++ app/src/main/java/com/example/reservant_mobile/ui/constants/Endpoints.kt
@@ -0,0 +1,8 @@
+package com.example.reservant_mobile.ui.constants
+
+const val LOGIN="/auth/login"
+const val LOGOUT="/auth/logout"
+const val REGISTER_CUSTOMER="/auth/register-customer"
+const val REGISTER_CUSTOMER_SUPPORT_AGENT="/auth/register-customer-support-agent"
+const val REGISTER_RESTAURANT_EMPLOYEE="/auth/register-restaurant-employee"
+const val REGISTER_RESTAURANT_OWNER="/auth/register-restaurant-owner"
