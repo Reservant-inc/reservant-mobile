bb32f82a70ca38f1dcd86a2ffa4c478b31e91269
Mroxny <50219282+Mroxny@users.noreply.github.com>
2024-03-21 23:43:11 +0100

    Refactor UserService data flow

--- app/src/main/java/com/example/reservant_mobile/data/services/UserService.kt
+++ app/src/main/java/com/example/reservant_mobile/data/services/UserService.kt
@@ -1,26 +1,34 @@
 package com.example.reservant_mobile.data.services
 
+import com.example.reservant_mobile.R
 import com.example.reservant_mobile.data.models.dtos.RegisterUserDTO
 import com.example.reservant_mobile.data.models.dtos.LoginCredentialsDTO
 
 
 interface IUserService{
-    suspend fun registerUser(user: RegisterUserDTO): Any?
-    suspend fun loginUser(credentials: LoginCredentialsDTO): Any?
+    suspend fun registerUser(user: RegisterUserDTO): Int
+    suspend fun loginUser(credentials: LoginCredentialsDTO): Boolean
 }
 
 class UserService(private var api: APIService = APIServiceImpl()) : IUserService {
 
-    override suspend fun registerUser(user: RegisterUserDTO): Any? {
+    /**
+     * @return -1 if everything is ok. Otherwise id of error string
+     */
+    override suspend fun registerUser(user: RegisterUserDTO): Int {
 //        TODO("point endpoint to /res/.../endpoints.xml")
-        val res = api.post(user,"/auth/register-customer") ?: return null
-        return res.status.value
+        val res = api.post(user,"/auth/register-customer") ?: return R.string.error_connection_server
+        if (res.status.value == 200) return -1
+//        TODO: return string ids based on ErrCode
+//        val j = JSONObject(res.body() as String)
+//        if(j.has("ErrCode")) {}
+        return R.string.error_register_username_taken
     }
 
-    override suspend fun loginUser(credentials: LoginCredentialsDTO): Any? {
+    override suspend fun loginUser(credentials: LoginCredentialsDTO): Boolean {
 //        TODO("point endpoint to /res/.../endpoints.xml")
-        val res = api.post(credentials,"/auth/login") ?: return null
-        return res.status.value
+        val res = api.post(credentials,"/auth/login") ?: return false
+        return res.status.value == 200
     }
 
 }
