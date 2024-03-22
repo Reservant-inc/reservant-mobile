eff1f95ab23b82ad9078d5aa0c62909528555d36
Aleksander Babij <aleksander.babij@gmail.com>
2024-03-22 11:17:02 +0100

    listof ints in userservice
    refactored enpoints to data object

--- app/src/main/java/com/example/reservant_mobile/data/services/UserService.kt
+++ app/src/main/java/com/example/reservant_mobile/data/services/UserService.kt
@@ -3,12 +3,11 @@ package com.example.reservant_mobile.data.services
 import com.example.reservant_mobile.R
 import com.example.reservant_mobile.data.models.dtos.RegisterUserDTO
 import com.example.reservant_mobile.data.models.dtos.LoginCredentialsDTO
-import com.example.reservant_mobile.ui.constants.*
-
+import com.example.reservant_mobile.ui.constants.Endpoints
 
 
 interface IUserService{
-    suspend fun registerUser(user: RegisterUserDTO): Int
+    suspend fun registerUser(user: RegisterUserDTO): List<Int>
     suspend fun loginUser(credentials: LoginCredentialsDTO): Boolean
 }
 
@@ -17,18 +16,20 @@ class UserService(private var api: APIService = APIServiceImpl()) : IUserService
     /**
      * @return -1 if everything is ok. Otherwise id of error string
      */
-    override suspend fun registerUser(user: RegisterUserDTO): Int {
-        val res = api.post(user, REGISTER_CUSTOMER) ?: return R.string.error_connection_server
-        if (res.status.value == 200) return -1
+    override suspend fun registerUser(user: RegisterUserDTO): List<Int> {
+        val res = api.post(user, Endpoints.REGISTER_CUSTOMER) ?: return listOf(R.string.error_connection_server)
+        if (res.status.value == 200) return listOf(-1)
 //        TODO: return string ids based on ErrCode
 //        val j = JSONObject(res.body() as String)
 //        if(j.has("ErrCode")) {}
-        return R.string.error_register_username_taken
+        return listOf(R.string.error_register_username_taken)
     }
 
     override suspend fun loginUser(credentials: LoginCredentialsDTO): Boolean {
-        val res = api.post(credentials, LOGIN) ?: return false
+        val res = api.post(credentials, Endpoints.LOGIN) ?: return false
         return res.status.value == 200
     }
+
+
 
 }
