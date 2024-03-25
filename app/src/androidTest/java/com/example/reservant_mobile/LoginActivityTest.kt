d7f5beb20976328800f93de0f9dc2182840d0e1a
Mroxny <50219282+Mroxny@users.noreply.github.com>
2024-03-25 21:28:51 +0100

    Update LoginActivityTest.kt

--- app/src/androidTest/java/com/example/reservant_mobile/LoginActivityTest.kt
+++ app/src/androidTest/java/com/example/reservant_mobile/LoginActivityTest.kt
@@ -1,6 +1,11 @@
 package com.example.reservant_mobile
 
 import androidx.activity.ComponentActivity
+import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.SemanticsMatcher
+import androidx.compose.ui.test.hasClickAction
+import androidx.compose.ui.test.hasSetTextAction
+import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
@@ -11,27 +16,63 @@ import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
+@OptIn(ExperimentalTestApi::class)
 class LoginActivityTest {
 
     @get:Rule
     val rule = createAndroidComposeRule<ComponentActivity>()
+
+    private lateinit var loginLabel: String
+    private lateinit var passwordLabel: String
+    private lateinit var signInLabel: String
+    private lateinit var error1: String
+    private lateinit var error2: String
+
+    private lateinit var loginField: SemanticsMatcher
+    private lateinit var passwordField: SemanticsMatcher
+    private lateinit var signInButton: SemanticsMatcher
+
 
     @Before
     fun setupLoginActivityNavHost() {
         rule.setContent {
             LoginActivity(navController = rememberNavController())
         }
+        this.loginLabel = rule.activity.getString(R.string.label_login)
+        this.passwordLabel = rule.activity.getString(R.string.label_password)
+        this.signInLabel = rule.activity.getString(R.string.label_signin)
+        this.error1 = rule.activity.getString(R.string.error_connection_server)
+        this.error2 = rule.activity.getString(R.string.error_login_wrong_credentials)
+
+        this.loginField = hasText(loginLabel) and hasSetTextAction()
+        this.passwordField = hasText(passwordLabel) and hasSetTextAction()
+        this.signInButton = hasText(signInLabel) and hasClickAction()
     }
 
+    @Test
+    fun enterEmpty_showError() {
+
+        rule.onNode(loginField).performTextInput("")
+        rule.onNode(passwordField).performTextInput("")
+        rule.onNode(signInButton).performClick()
+
+        rule.onNodeWithText(error2).assertExists()
+    }
 
     @Test
-    fun wrongCredentialsReturnErrorCode() {
-//        val loginLabel = rule.activity.getString(R.string.label_login)
+    fun enterInvalidUsername_showError() {
+        rule.onNode(loginField).performTextInput("invalid")
+        rule.onNode(passwordField).performTextInput("Pa${'$'}${'$'}w0rd")
+        rule.onNode(signInButton).performClick()
 
-        rule.onNodeWithText("Login").performTextInput("test")
-        rule.onNodeWithText("Password").performTextInput("123123123")
+        rule.waitUntilExactlyOneExists(hasText(error2), 5000)
+    }
+    @Test
+    fun enterInvalidPassword_showError() {
+        rule.onNode(loginField).performTextInput("john@doe.pl")
+        rule.onNode(passwordField).performTextInput("invalid")
+        rule.onNode(signInButton).performClick()
 
-        rule.onNodeWithText("login").performClick()
-        rule.onNodeWithText("login").assertExists()
+        rule.waitUntilExactlyOneExists(hasText(error2), 5000)
     }
 }
