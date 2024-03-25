4d285440c737c909028fdf018e3da0a960da1d14
Mroxny <50219282+Mroxny@users.noreply.github.com>
2024-03-25 12:57:40 +0100

    Update LoginActivityTest.kt

--- app/src/androidTest/java/com/example/reservant_mobile/LoginActivityTest.kt
+++ app/src/androidTest/java/com/example/reservant_mobile/LoginActivityTest.kt
@@ -1,13 +1,11 @@
 package com.example.reservant_mobile
 
 import androidx.activity.ComponentActivity
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performTextInput
-import androidx.navigation.compose.ComposeNavigator
-import androidx.navigation.testing.TestNavHostController
+import androidx.navigation.compose.rememberNavController
 import com.example.reservant_mobile.ui.activities.LoginActivity
 import org.junit.Before
 import org.junit.Rule
@@ -18,19 +16,16 @@ class LoginActivityTest {
     @get:Rule
     val rule = createAndroidComposeRule<ComponentActivity>()
 
-    private lateinit var navController: TestNavHostController
-
     @Before
     fun setupLoginActivityNavHost() {
         rule.setContent {
-            navController = TestNavHostController(LocalContext.current)
-            navController.navigatorProvider.addNavigator(ComposeNavigator())
-            LoginActivity(navController = navController)
+            LoginActivity(navController = rememberNavController())
         }
     }
 
+
     @Test
-    fun reservantNavHost_verifyStartDestination() {
+    fun wrongCredentialsReturnErrorCode() {
 //        val loginLabel = rule.activity.getString(R.string.label_login)
 
         rule.onNodeWithText("Login").performTextInput("test")
