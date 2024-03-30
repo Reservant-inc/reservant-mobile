64bdcd4e318224470b1021a3d41128312402fe9a
Mroxny <50219282+Mroxny@users.noreply.github.com>
2024-03-30 14:25:25 +0100

    Add LocalService provider

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -88,5 +88,5 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
     implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.0")
     implementation ("androidx.compose.material:material-icons-extended:1.6.4")
-
+    implementation ("androidx.datastore:datastore-preferences:1.0.0")
 }
---
+++ app/src/main/java/com/example/reservant_mobile/data/services/LocalService.kt
@@ -0,0 +1,44 @@
+package com.example.reservant_mobile.data.services
+
+import android.content.Context
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.firstOrNull
+
+
+class LocalService{
+    private val context  = ApplicationService.instance
+    private val Context.dataStore by preferencesDataStore(context.packageName)
+
+    suspend fun saveBearerToken(bearerToken: String) {
+        context.dataStore.edit {
+            it[bearerTokenKey] = bearerToken
+        }
+    }
+
+    suspend fun saveRefreshToken(refreshToken: String) {
+        context.dataStore.edit {
+            it[refreshTokenKey] = refreshToken
+        }
+    }
+
+    suspend fun getBearerToken(): String {
+        return try {
+            context.dataStore.data.first()[bearerTokenKey].toString()
+        } catch (_: NoSuchElementException){
+            ""
+        }
+    }
+
+    suspend fun getRefreshToken(): String {
+        return try {
+            context.dataStore.data.first()[refreshTokenKey].toString()
+        } catch (_: NoSuchElementException){
+            ""
+        }    }
+
+    private val bearerTokenKey = stringPreferencesKey("bearer_token")
+    private val refreshTokenKey = stringPreferencesKey("refresh_token")
+}
