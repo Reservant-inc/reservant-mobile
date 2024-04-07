6f647f5397881e161658b07fe6bc111e635e658b
Michał Wiśniewski <s24845@pjwstk.edu.pl>
2024-04-07 18:01:11 +0200

    Update RegisterRestaurantActivity.kt

--- app/src/main/java/com/example/reservant_mobile/ui/activities/RegisterRestaurantActivity.kt
+++ app/src/main/java/com/example/reservant_mobile/ui/activities/RegisterRestaurantActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
@@ -130,7 +131,7 @@ fun RegisterRestaurantActivity(navController: NavHostController) {
             }
         )
 
-        Spacer(modifier = Modifier.weight(1f))
+        Spacer(modifier = Modifier.height(16.dp))
 
         Row(
             modifier = Modifier.fillMaxWidth(),
