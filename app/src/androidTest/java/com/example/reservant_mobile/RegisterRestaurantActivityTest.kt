e6bac95ae91c73fad8f23d31a6a9227ea4dcc40f
Michał Wiśniewski <s24845@pjwstk.edu.pl>
2024-04-07 19:31:34 +0200

    Added simple input test

---
+++ app/src/androidTest/java/com/example/reservant_mobile/RegisterRestaurantActivityTest.kt
@@ -0,0 +1,52 @@
+package com.example.reservant_mobile
+
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.hasTestTag
+import androidx.compose.ui.test.hasText
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performTextInput
+import androidx.navigation.compose.ComposeNavigator
+import androidx.navigation.testing.TestNavHostController
+import com.example.reservant_mobile.ui.activities.RegisterActivity
+import com.example.reservant_mobile.ui.activities.RegisterRestaurantActivity
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+
+class RegisterRestaurantActivityTest {
+
+    @get:Rule
+    val rule = createAndroidComposeRule<ComponentActivity>()
+
+    private lateinit var navController: TestNavHostController
+
+    @Before
+    fun setupRegisterActivityNavHost() {
+        rule.setContent {
+            navController = TestNavHostController(LocalContext.current)
+            navController.navigatorProvider.addNavigator(ComposeNavigator())
+            RegisterRestaurantActivity(navController = navController)
+        }
+    }
+
+    @Test
+    fun enterInput() {
+        rule.onNodeWithText("Name").performTextInput("Test name")
+        rule.onNodeWithText("NIP").performTextInput("1234567890")
+
+        rule.onNodeWithText("Restaurant type").performClick()
+        rule.onNodeWithText("Bar").performClick()
+
+        rule.onNodeWithText("Address").performTextInput("Mickiewicza 9")
+        rule.onNodeWithText("Postal code").performTextInput("02-234")
+        rule.onNodeWithText("City").performTextInput("Warsaw")
+
+        rule.onNodeWithText("Register Restaurant").performClick()
+    }
+
+}
