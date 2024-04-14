9fef554130307684a0d63f9ce96633c9cb38efaa
Mroxny <50219282+Mroxny@users.noreply.github.com>
2024-04-15 00:24:03 +0200

    Implement Result<>

--- app/src/main/java/com/example/reservant_mobile/data/services/RestaurantService.kt
+++ app/src/main/java/com/example/reservant_mobile/data/services/RestaurantService.kt
@@ -3,62 +3,101 @@ package com.example.reservant_mobile.data.services
 import com.example.reservant_mobile.R
 import com.example.reservant_mobile.data.models.dtos.RegisterRestaurantDTO
 import com.example.reservant_mobile.data.models.dtos.RestaurantDTO
+import com.example.reservant_mobile.data.models.dtos.fields.Result
 import com.example.reservant_mobile.ui.constants.Endpoints
 import io.ktor.client.call.body
 
 class RestaurantService(private var api: APIService = APIServiceImpl()) {
 
-    suspend fun registerRestaurant(restaurant: RegisterRestaurantDTO): List<Int> {
-        val res = api.post(restaurant, Endpoints.MY_RESTAURANTS) ?: return listOf(R.string.error_connection_server)
-        if (res.status.value == 200) return listOf(-1)
-        if (res.status.value == 401) return listOf(R.string.error_unauthorized_access)
+    suspend fun registerRestaurant(restaurant: RegisterRestaurantDTO): Result<Boolean> {
+        val res = api.post(restaurant, Endpoints.MY_RESTAURANTS) ?:
+            return Result(true, mapOf(pair= Pair("TOAST", R.string.error_connection_server)), false)
 
-        return listOf(R.string.error_unknown)
+        if (res.status.value == 200)
+            return Result(isError = false, value = true)
+
+        if (res.status.value == 401)
+            return Result(isError = true, errors = mapOf(pair= Pair("TOAST", R.string.error_unauthorized_access)) ,value = false)
+
+
+        return Result(true, mapOf(pair = Pair("TOAST", R.string.error_unknown)), false)
+
     }
 
-    suspend fun getRestaurants():String {
-        val res = api.get(Endpoints.MY_RESTAURANTS)?: return "error_connection_server"
+    suspend fun getRestaurants():Result<List<RestaurantDTO>?>  {
+        val res = api.get(Endpoints.MY_RESTAURANTS) ?:
+            return Result(true, mapOf(pair= Pair("TOAST", R.string.error_connection_server)), null)
+
 
         if (res.status.value == 200){
-            val j:List<RestaurantDTO> = res.body()
-            return j.toString()
+            return try {
+                val j:List<RestaurantDTO> = res.body()
+                Result(isError = false, value = j)
+            }
+            catch (e: Exception){
+                Result(isError = true, errors = mapOf(pair= Pair("TOAST", R.string.error_unknown)) ,value = null)
+            }
         }
-        if (res.status.value == 401) return "unauthorized"
 
-        return "error_unknown"
+        if (res.status.value == 401)
+            return Result(isError = true, errors = mapOf(pair= Pair("TOAST", R.string.error_unauthorized_access)) ,value = null)
 
+
+        return Result(true, mapOf(pair = Pair("TOAST", R.string.error_unknown)), null)
     }
-    suspend fun getRestaurant(id:Any): String {
-        val res = api.get(Endpoints.MY_RESTAURANT.replace("{id}",id.toString())) ?: return "error_connection_server"
+    suspend fun getRestaurant(id:Any): Result<RestaurantDTO?>  {
+        val res = api.get(Endpoints.MY_RESTAURANT.replace("{id}",id.toString())) ?:
+            return Result(true, mapOf(pair= Pair("TOAST", R.string.error_connection_server)), null)
+
 
         if (res.status.value == 200){
-            val j:RestaurantDTO = res.body()
-            return j.toString()
+            return try {
+                val j:RestaurantDTO = res.body()
+                Result(isError = false, value = j)
+            }
+            catch (e: Exception){
+                Result(isError = true, errors = mapOf(pair= Pair("TOAST", R.string.error_unknown)) ,value = null)
+            }
         }
-        if (res.status.value == 401) return "unauthorized"
 
-        return "error_unknown"
+        if (res.status.value == 401)
+            return Result(isError = true, errors = mapOf(pair= Pair("TOAST", R.string.error_unauthorized_access)) ,value = null)
+
+
+        return Result(true, mapOf(pair = Pair("TOAST", R.string.error_unknown)), null)
 
     }
 
-    suspend fun editRestaurant(restaurant: RegisterRestaurantDTO): List<Int> {
-//        TODO: Implement edit
-        /*val res = api.post(restaurant, Endpoints.MY_RESTAURANTS) ?: return listOf(R.string.error_connection_server)
-        if (res.status.value == 200) return listOf(-1)
-        if (res.status.value == 401) return listOf(R.string.error_unauthorized_access)
+    private suspend fun editRestaurant(id: Any, restaurant: RestaurantDTO): Result<Boolean>  {
+//        TODO: Implement edit (add put)
+        val res = api.post( restaurant ,Endpoints.MY_RESTAURANT.replace("{id}",id.toString())) ?:
+        return Result(true, mapOf(pair= Pair("TOAST", R.string.error_connection_server)), false)
 
-        return listOf(R.string.error_unknown)*/
-        return listOf(0)
+
+        if (res.status.value == 200)
+            return Result(isError = false, value = true)
+
+        if (res.status.value == 401)
+            return Result(isError = true, errors = mapOf(pair= Pair("TOAST", R.string.error_unauthorized_access)) ,value = false)
+
+
+        return Result(true, mapOf(pair = Pair("TOAST", R.string.error_unknown)), false)
     }
 
-    suspend fun deleteRestaurant(id: Int): List<Int> {
+    private suspend fun deleteRestaurant(id: Int): Result<Boolean>  {
 //        TODO: Implement delete
-        /*val res = api.post(restaurant, Endpoints.MY_RESTAURANTS) ?: return listOf(R.string.error_connection_server)
-        if (res.status.value == 200) return listOf(-1)
-        if (res.status.value == 401) return listOf(R.string.error_unauthorized_access)
+        val res = api.get(Endpoints.MY_RESTAURANT.replace("{id}",id.toString())) ?:
+        return Result(true, mapOf(pair= Pair("TOAST", R.string.error_connection_server)), false)
 
-        return listOf(R.string.error_unknown)*/
-        return listOf(0)
+
+        if (res.status.value == 200)
+            return Result(isError = false, value = true)
+
+        if (res.status.value == 401)
+            return Result(isError = true, errors = mapOf(pair= Pair("TOAST", R.string.error_unauthorized_access)) ,value = false)
+
+
+        return Result(true, mapOf(pair = Pair("TOAST", R.string.error_unknown)), false)
     }
 
 }
