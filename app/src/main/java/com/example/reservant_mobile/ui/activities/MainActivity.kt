69e2ce0e68ee2a16aabdd59ce7db498104ab25b0
Mroxny <50219282+Mroxny@users.noreply.github.com>
2024-04-14 09:47:00 +0200

    Update MainActivity.kt

--- app/src/main/java/com/example/reservant_mobile/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/reservant_mobile/ui/activities/MainActivity.kt
@@ -5,34 +5,49 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.reservant_mobile.ui.viewmodels.LoginViewModel
+import kotlinx.coroutines.launch
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            val navController = rememberNavController()
-            NavHost(navController = navController, startDestination = "landing") {
-                composable(route = "landing") {
-                    LandingActivity(navController = navController)
-                }
-                composable(route = "login") {
-                    LoginActivity(navController = navController)
-                }
-                composable(route = "register") {
-                    RegisterActivity(navController = navController)
-                }
-                composable(route = "home") {
-                    HomeActivity(navController = navController)
+
+        val splashScreen = installSplashScreen()
+        var startPoint = String()
+        splashScreen.setKeepOnScreenCondition { startPoint.isEmpty() }
+
+        lifecycleScope.launch {
+            startPoint = if(LoginViewModel().refreshToken())
+                "home"
+            else
+                "landing"
+
+            setContent {
+                val navController = rememberNavController()
+                NavHost(navController = navController, startDestination = startPoint) {
+                    composable(route = "landing") {
+                        LandingActivity(navController = navController)
+                    }
+                    composable(route = "login") {
+                        LoginActivity(navController = navController)
+                    }
+                    composable(route = "register") {
+                        RegisterActivity(navController = navController)
+                    }
+                    composable(route = "home") {
+                        HomeActivity(navController = navController)
+                    }
+
                 }
 
             }
-
         }
-
     }
 }
 
