270d0dfb4ad35e70015ad145a7ac2e2513d5316d
Michał Wiśniewski <s24845@pjwstk.edu.pl>
2024-04-17 09:16:38 +0200

    Changed padding

--- app/src/main/java/com/example/reservant_mobile/ui/activities/RestaurantManagementActivity.kt
+++ app/src/main/java/com/example/reservant_mobile/ui/activities/RestaurantManagementActivity.kt
@@ -2,18 +2,22 @@ package com.example.reservant_mobile.ui.activities
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import com.example.reservant_mobile.R
 import com.example.reservant_mobile.ui.components.LogoWithReturn
 import com.example.reservant_mobile.ui.components.UserButton
 
@@ -29,34 +33,44 @@ fun RestaurantManagementActivity(navController: NavHostController) {
         horizontalAlignment = Alignment.Start
     ) {
         LogoWithReturn(navController)
-
+        
         Text(
-            text = "Nazwa restauracji"
+            text = "Nazwa restauracji",
+            style = MaterialTheme.typography.headlineLarge,
+            modifier = Modifier
+            .padding(top = 32.dp)
+            .padding(bottom = 32.dp)
         )
 
         UserButton(
-            label = "Edytuj dane lokalu",
-            onClick = { /*TODO*/ }
+            label = stringResource(R.string.label_management_edit_local_data),
+            onClick = { /*TODO*/ },
+            modifier = Modifier.padding(top = 16.dp)
         )
         UserButton(
-            label = "Zarządzaj menu",
-            onClick = { /*TODO*/ }
+            label = stringResource(R.string.label_management_manage_menu),
+            onClick = { /*TODO*/ },
+            modifier = Modifier.padding(top = 16.dp)
         )
         UserButton(
-            label = "Opinie",
-            onClick = { /*TODO*/ }
+            label = stringResource(R.string.label_management_reviews),
+            onClick = { /*TODO*/ },
+            modifier = Modifier.padding(top = 16.dp)
         )
         UserButton(
-            label = "Zarzadzaj pracownikami",
-            onClick = { /*TODO*/ }
+            label = stringResource(R.string.label_management_reviews),
+            onClick = { /*TODO*/ },
+            modifier = Modifier.padding(top = 16.dp)
         )
         UserButton(
-            label = "Zarzadzaj subskrypcja",
-            onClick = { /*TODO*/ }
+            label = stringResource(R.string.label_management_manage_subscription),
+            onClick = { /*TODO*/ },
+            modifier = Modifier.padding(top = 16.dp)
         )
         UserButton(
-            label = "Usuń lokal",
-            onClick = { /*TODO*/ }
+            label = stringResource(R.string.label_management_delete_local),
+            onClick = { /*TODO*/ },
+            modifier = Modifier.padding(top = 16.dp)
         )
 
 
