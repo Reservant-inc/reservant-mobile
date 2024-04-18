749f9eb06093035a79ba64c48f85fe05bfe03ac9
Mroxny <50219282+Mroxny@users.noreply.github.com>
2024-04-18 19:13:00 +0200

    General refactor
    Co-Authored-By: Aleksander Babij <108064906+Bambi54@users.noreply.github.com>

--- app/src/main/java/com/example/reservant_mobile/data/utils/GetFileFromURIUtil.kt
+++ app/src/main/java/com/example/reservant_mobile/data/utils/GetFileFromURIUtil.kt
@@ -5,26 +5,23 @@ import android.content.Context
 import android.net.Uri
 import java.io.ByteArrayOutputStream
 
-class GetFileFromURIUtil {
+fun getFileFromUri(context: Context, uri: Uri): ByteArray? {
+    val contentResolver: ContentResolver = context.contentResolver
+    val outputStream = ByteArrayOutputStream()
 
-    fun getFileDataFromUri(context: Context, uri: Uri): ByteArray? {
-        val contentResolver: ContentResolver = context.contentResolver
-        val outputStream = ByteArrayOutputStream()
-
-        try {
-            contentResolver.openInputStream(uri)?.use { inputStream ->
-                val buffer = ByteArray(4 * 1024) // buffer size
-                var read: Int
-                while (inputStream.read(buffer).also { read = it } != -1) {
-                    outputStream.write(buffer, 0, read)
-                }
-                outputStream.flush()
+    try {
+        contentResolver.openInputStream(uri)?.use { inputStream ->
+            val buffer = ByteArray(4 * 1024) // buffer size
+            var read: Int
+            while (inputStream.read(buffer).also { read = it } != -1) {
+                outputStream.write(buffer, 0, read)
             }
-        } catch (e: Exception) {
-            e.printStackTrace()
-            return null
+            outputStream.flush()
         }
-
-        return outputStream.toByteArray()
+    } catch (e: Exception) {
+        e.printStackTrace()
+        return null
     }
+
+    return outputStream.toByteArray()
 }
