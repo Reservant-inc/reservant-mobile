24ed95e75de66d7a2bbbfff1345ee9b3db536f2c
Aleksander Babij <aleksander.babij@gmail.com>
2024-04-18 18:45:40 +0200

    cancel unique-login request on value change
    Co-Authored-By: Maciej Mrozek <50219282+Mroxny@users.noreply.github.com>

--- app/src/main/java/com/example/reservant_mobile/ui/activities/RegisterActivity.kt
+++ app/src/main/java/com/example/reservant_mobile/ui/activities/RegisterActivity.kt
@@ -40,8 +40,10 @@ import com.example.reservant_mobile.ui.components.MyDatePickerDialog
 import com.example.reservant_mobile.ui.components.ShowErrorToast
 import com.example.reservant_mobile.ui.components.UserButton
 import com.example.reservant_mobile.ui.viewmodels.RegisterViewModel
+import kotlinx.coroutines.cancelChildren
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlin.coroutines.coroutineContext
 
 @Composable
 fun RegisterActivity(navController: NavHostController) {
@@ -65,6 +67,7 @@ fun RegisterActivity(navController: NavHostController) {
             inputText = registerViewModel.login.value,
             onValueChange = {
                 registerViewModel.login.value = it
+                registerViewModel.viewModelScope.coroutineContext.cancelChildren()
                 registerViewModel.viewModelScope.launch {
                     delay(1000)
                     registerViewModel.checkLoginUnique()
