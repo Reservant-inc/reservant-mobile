eb343bb29a7cd1898d56b430793341e7b586112f
Mroxny <50219282+Mroxny@users.noreply.github.com>
2024-04-20 20:33:53 +0200

    Add restaurant endpoints

--- app/src/main/java/com/example/reservant_mobile/ui/constants/Endpoints.kt
+++ app/src/main/java/com/example/reservant_mobile/ui/constants/Endpoints.kt
@@ -9,10 +9,15 @@ data object Endpoints {
     const val REFRESH_ACCESS_TOKEN="/refresh-token"
     const val REGISTER_CUSTOMER="/auth/register-customer"
     const val REGISTER_CUSTOMER_SUPPORT_AGENT="/auth/register-customer-support-agent"
-    const val REGISTER_RESTAURANT_EMPLOYEE="/auth/register-restaurant-employee"
     const val REGISTER_RESTAURANT_OWNER="/auth/register-restaurant-owner"
     const val MY_RESTAURANTS="/my-restaurants"
     fun MY_RESTAURANT(id: String) = "/my-restaurants/$id"
+    fun MY_RESTAURANT_EMPLOYEES(id: String) = "/my-restaurants/$id/employees"
+    fun MOVE_RESTAURANT_TO_GROUP(id: String) = "/my-restaurants/$id/move-to-group"
+
+    const val MY_RESTAURANT_GROUPS="/my-restaurant-groups"
+    fun MY_RESTAURANT_GROUP(id: String) = "/my-restaurant-groups/$id"
+
     const val FILE_UPLOADS="/uploads"
 
 }
