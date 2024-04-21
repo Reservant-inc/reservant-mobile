2f2bed8a472a0765a77c77c00b70f8b9e1fe76f3
Mroxny <50219282+Mroxny@users.noreply.github.com>
2024-04-21 16:47:28 +0200

    Add menu and menuItems

--- app/src/main/java/com/example/reservant_mobile/ui/constants/Endpoints.kt
+++ app/src/main/java/com/example/reservant_mobile/ui/constants/Endpoints.kt
@@ -1,8 +1,8 @@
 package com.example.reservant_mobile.ui.constants
 
 data object Endpoints {
-
     const val BACKEND_URL="http://172.21.40.127:12038"
+    const val FILE_UPLOADS="/uploads"
     const val LOGIN="/auth/login"
     const val LOGOUT="/auth/logout"
     const val LOGIN_UNIQUE="/auth/is-unique-login"
@@ -13,6 +13,10 @@ data object Endpoints {
     const val REGISTER_RESTAURANT_OWNER="/auth/register-restaurant-owner"
     const val MY_RESTAURANTS="/my-restaurants"
     fun MY_RESTAURANT(id: String) = "/my-restaurants/$id"
-    const val FILE_UPLOADS="/uploads"
+    fun RESTAURANT_MENUS(id: String) = "/my-restaurants/$id/menus"
+    fun RESTAURANT_MENU(restaurantId: String, menuId:String) = "/my-restaurants/$restaurantId/menus/$menuId"
+    fun RESTAURANT_MENU_ITEMS(id: String) = "/my-restaurants/$id/menu-items"
+    fun RESTAURANT_MENU_ITEM(restaurantId: String, itemId:String) = "/my-restaurants/$restaurantId/menu-items/$itemId"
+
 
 }
