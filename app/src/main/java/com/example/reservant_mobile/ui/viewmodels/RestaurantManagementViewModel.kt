597e3e1c32699d4b58e88005b045f03d449d7f43
Michał Wiśniewski <s24845@pjwstk.edu.pl>
2024-04-21 12:43:34 +0200

    Added ViewModel

---
+++ app/src/main/java/com/example/reservant_mobile/ui/viewmodels/RestaurantManagementViewModel.kt
@@ -0,0 +1,48 @@
+package com.example.reservant_mobile.ui.viewmodels
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.reservant_mobile.data.models.dtos.RestaurantDTO
+import com.example.reservant_mobile.data.services.IRestaurantService
+import com.example.reservant_mobile.data.services.RestaurantService
+import kotlinx.coroutines.launch
+
+
+class RestaurantManagementViewModel(private val restaurantService: IRestaurantService = RestaurantService()) : ViewModel() {
+
+
+    var restaurants: List<RestaurantDTO>? by mutableStateOf(listOf())
+    private var selectedRestaurant: RestaurantDTO? by mutableStateOf(null)
+
+    init {
+        loadRestaurants()
+    }
+
+    private fun loadRestaurants() {
+        viewModelScope.launch {
+            restaurants = restaurantService.getRestaurants().value
+        }
+    }
+
+    fun selectRestaurant(restaurant: RestaurantDTO) {
+        selectedRestaurant = restaurant
+    }
+
+    suspend fun getSingleRestaurant(id: Int): RestaurantDTO? {
+        return restaurantService.getRestaurant(id).value
+    }
+
+    // TODO: implement delete
+    fun deleteSelectedRestaurant() {
+        selectedRestaurant?.let { restaurant ->
+            viewModelScope.launch {
+                restaurantService.deleteRestaurant(restaurant.id)
+                loadRestaurants()
+            }
+        }
+    }
+
+}
