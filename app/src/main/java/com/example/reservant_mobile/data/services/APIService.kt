1c663332b9690e0c0cab6a36be218e0e35a312fc
Mroxny <50219282+Mroxny@users.noreply.github.com>
2024-04-24 00:07:48 +0200

    Update APIService.kt

--- app/src/main/java/com/example/reservant_mobile/data/services/APIService.kt
+++ app/src/main/java/com/example/reservant_mobile/data/services/APIService.kt
@@ -1,6 +1,8 @@
 package com.example.reservant_mobile.data.services
 
+import com.example.reservant_mobile.R
 import com.example.reservant_mobile.data.models.dtos.TokenDTO
+import com.example.reservant_mobile.data.models.dtos.fields.Result
 import com.example.reservant_mobile.ui.constants.Endpoints
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
@@ -9,7 +11,6 @@ import io.ktor.client.plugins.auth.Auth
 import io.ktor.client.plugins.auth.providers.BearerTokens
 import io.ktor.client.plugins.auth.providers.bearer
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.client.plugins.cookies.HttpCookies
 import io.ktor.client.plugins.defaultRequest
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
@@ -25,18 +26,19 @@ import io.ktor.client.request.setBody
 import io.ktor.client.request.url
 import io.ktor.client.statement.HttpResponse
 import io.ktor.http.ContentType
+import io.ktor.http.HttpStatusCode
 import io.ktor.http.contentType
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.Serializable
 import kotlin.Exception
 
 interface APIService{
-    suspend fun get(endpoint: String = ""): HttpResponse?
-    suspend fun post(obj: @Serializable Any, endpoint: String = ""): HttpResponse?
-    suspend fun put(obj: @Serializable Any, endpoint: String): HttpResponse?
-    suspend fun delete(endpoint: String): HttpResponse?
-
+    suspend fun get(endpoint: String = ""): Result<HttpResponse?>
+    suspend fun post(obj: @Serializable Any, endpoint: String = ""): Result<HttpResponse?>
+    suspend fun put(obj: @Serializable Any, endpoint: String): Result<HttpResponse?>
+    suspend fun delete(endpoint: String): Result<HttpResponse?>
     suspend fun getHttpClient(): HttpClient
+    suspend fun responseWrapper(res: HttpResponse?): Result<HttpResponse?>
 }
 
 
@@ -65,69 +67,78 @@ class APIServiceImpl: APIService {
                         localService.getRefreshToken()
                     )
                 }
-                refreshTokens {
-                    try {
-                        val token: TokenDTO = client.get {
-                            markAsRefreshTokenRequest()
-                            url(Endpoints.REFRESH_ACCESS_TOKEN)
-                            parameter("refreshToken", localService.getRefreshToken())
-                        }.body()
-                        BearerTokens(
-                            accessToken = token.token,
-                            refreshToken = token.token
-                        )
-                    }
-                    catch (e: Exception){
-                        println("[REFRESH TOKEN ERROR]: "+e.message)
-                        null
-                    }
+            }
+        }
 
+    }
+
+
+    override suspend fun get(endpoint: String): Result<HttpResponse?> {
+        return responseWrapper(
+            try {
+                client.get(endpoint)
+            } catch (e: Exception){
+                println("[GET ERROR]: "+e.message)
+                null
+            }
+        )
+    }
+
+    override suspend fun post(obj: @Serializable Any, endpoint: String): Result<HttpResponse?> {
+        return responseWrapper(
+            try {
+                client.post(endpoint) {
+                    setBody(obj)
                 }
+            } catch (e: Exception){
+                println("[POST ERROR]: "+e.message)
+                null
             }
-        }
-
-    }
-    override suspend fun get(endpoint: String): HttpResponse? {
-        return try {
-            client.get(endpoint)
-        } catch (e: Exception){
-            println("[GET ERROR]: "+e.message)
-            null
-        }
+        )
     }
 
-    override suspend fun post(obj: @Serializable Any, endpoint: String): HttpResponse? {
-        return try {
-            client.post(endpoint) {
+    override suspend fun put(obj: @Serializable Any, endpoint: String): Result<HttpResponse?> {
+        return responseWrapper(
+            try {
+                client.put(endpoint) {
                 setBody(obj)
             }
-        } catch (e: Exception){
-            println("[POST ERROR]: "+e.message)
-            null
-        }
-    }
-
-    override suspend fun put(obj: @Serializable Any, endpoint: String): HttpResponse? {
-        return try {
-            client.put(endpoint) {
-                setBody(obj)
+            } catch (e: Exception){
+                println("[PUT ERROR]: "+e.message)
+                null
             }
-        } catch (e: Exception){
-            println("[PUT ERROR]: "+e.message)
-            null
-        }
+        )
     }
 
-    override suspend fun delete(endpoint: String): HttpResponse? {
-        return try {
-            client.delete(endpoint)
-        } catch (e: Exception){
-            println("[DELETE ERROR]: "+e.message)
-            null
-        }
+    override suspend fun delete(endpoint: String): Result<HttpResponse?> {
+        return responseWrapper(
+            try {
+                client.delete(endpoint)
+            } catch (e: Exception){
+                println("[DELETE ERROR]: "+e.message)
+                null
+            }
+        )
     }
 
     override suspend fun getHttpClient(): HttpClient {
         return client
+    }
+
+    override suspend fun responseWrapper(res: HttpResponse?): Result<HttpResponse?> {
+        res?:
+            return Result(isError = true, errors =  mapOf(pair= Pair("TOAST", R.string.error_connection_server)), value = null)
+
+        if (res.status == HttpStatusCode.Unauthorized)
+            return Result(isError = true, errors = mapOf(pair= Pair("TOAST", R.string.error_unauthorized_access)) ,value = null)
+
+        if (res.status == HttpStatusCode.BadRequest)
+            return Result(isError = true, errors = mapOf(pair= Pair("TOAST", R.string.error_unauthorized_access)) ,value = null)
+
+        if (res.status == HttpStatusCode.NotFound)
+            return Result(isError = true, errors = mapOf(pair= Pair("TOAST", R.string.error_unauthorized_access)) ,value = null)
+
+        return Result(isError = false, value = res)
+
     }
 }
