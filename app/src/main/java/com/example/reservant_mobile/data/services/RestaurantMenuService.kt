cf1f76bc7c3c0ed88bd0cd0311bcee770d04e03a
Mroxny <50219282+Mroxny@users.noreply.github.com>
2024-04-24 00:08:01 +0200

    Update RestaurantMenuService.kt

--- app/src/main/java/com/example/reservant_mobile/data/services/RestaurantMenuService.kt
+++ app/src/main/java/com/example/reservant_mobile/data/services/RestaurantMenuService.kt
@@ -15,7 +15,6 @@ interface IRestaurantMenuService{
     suspend fun addMenu(restaurantId: Any, menu: RestaurantMenuDTO): Result<RestaurantMenuDTO?>
     suspend fun getMenus(restaurantId:Any): Result<List<RestaurantMenuDTO>?>
     suspend fun getMenu(restaurantId:Any, menuId: Any): Result<RestaurantMenuDTO?>
-
     suspend fun createMenuItems(restaurantId:Any, menuItems: List<RestaurantMenuItemDTO>): Result<List<RestaurantMenuItemDTO>?>
     suspend fun getMenuItems(restaurantId:Any): Result<List<RestaurantMenuItemDTO>?>
     suspend fun getMenuItem(restaurantId:Any, itemId:Any): Result<RestaurantMenuItemDTO?>
@@ -23,132 +22,110 @@ interface IRestaurantMenuService{
 }
 class RestaurantMenuService(private var api: APIService = APIServiceImpl()): IRestaurantMenuService {
     override suspend fun addMenu(restaurantId: Any, menu: RestaurantMenuDTO): Result<RestaurantMenuDTO?>{
-        val res = api.post(menu, Endpoints.RESTAURANT_MENUS(restaurantId.toString())) ?:
-        return Result(true, mapOf(pair= Pair("TOAST", R.string.error_connection_server)), null)
+        val res = api.post(menu, Endpoints.RESTAURANT_MENUS(restaurantId.toString()))
 
-        if (res.status == HttpStatusCode.OK){
+        if(res.isError)
+            return Result(isError = true, errors = res.errors, value = null)
+
+        if (res.value!!.status == HttpStatusCode.OK){
             return try {
-                val j:RestaurantMenuDTO = res.body()
-                Result(isError = false, value = j)
+                Result(isError = false, value = res.value.body())
             }
             catch (e: Exception){
                 Result(isError = true, errors = mapOf(pair= Pair("TOAST", R.string.error_unknown)) ,value = null)
             }
         }
-
-        if (res.status == HttpStatusCode.Unauthorized)
-            return Result(isError = true, errors = mapOf(pair= Pair("TOAST", R.string.error_unauthorized_access)) ,value = null)
-
 
         return Result(true, mapOf(pair = Pair("TOAST", R.string.error_unknown)), null)
     }
 
     override suspend fun getMenus(restaurantId: Any): Result<List<RestaurantMenuDTO>?> {
-        val res = api.get(Endpoints.RESTAURANT_MENUS(restaurantId.toString())) ?:
-        return Result(true, mapOf(pair= Pair("TOAST", R.string.error_connection_server)), null)
+        val res = api.get(Endpoints.RESTAURANT_MENUS(restaurantId.toString()))
 
+        if(res.isError)
+            return Result(isError = true, errors = res.errors, value = null)
 
-        if (res.status == HttpStatusCode.OK){
+        if (res.value!!.status == HttpStatusCode.OK){
             return try {
-                val j:List<RestaurantMenuDTO> = res.body()
-                Result(isError = false, value = j)
+                Result(isError = false, value =  res.value.body())
             }
             catch (e: Exception){
                 Result(isError = true, errors = mapOf(pair= Pair("TOAST", R.string.error_unknown)) ,value = null)
             }
         }
-
-        if (res.status == HttpStatusCode.Unauthorized)
-            return Result(isError = true, errors = mapOf(pair= Pair("TOAST", R.string.error_unauthorized_access)) ,value = null)
-
 
         return Result(true, mapOf(pair = Pair("TOAST", R.string.error_unknown)), null)
     }
 
     override suspend fun getMenu(restaurantId: Any, menuId: Any): Result<RestaurantMenuDTO?> {
-        val res = api.get(Endpoints.RESTAURANT_MENU(restaurantId.toString(), menuId.toString())) ?:
-        return Result(true, mapOf(pair= Pair("TOAST", R.string.error_connection_server)), null)
+        val res = api.get(Endpoints.RESTAURANT_MENU(restaurantId.toString(), menuId.toString()))
 
+        if(res.isError)
+            return Result(isError = true, errors = res.errors, value = null)
 
-        if (res.status == HttpStatusCode.OK){
+        if (res.value!!.status == HttpStatusCode.OK){
             return try {
-                val j:RestaurantMenuDTO = res.body()
-                Result(isError = false, value = j)
+                Result(isError = false, value =  res.value.body())
             }
             catch (e: Exception){
                 Result(isError = true, errors = mapOf(pair= Pair("TOAST", R.string.error_unknown)) ,value = null)
             }
         }
-
-        if (res.status == HttpStatusCode.Unauthorized)
-            return Result(isError = true, errors = mapOf(pair= Pair("TOAST", R.string.error_unauthorized_access)) ,value = null)
-
 
         return Result(true, mapOf(pair = Pair("TOAST", R.string.error_unknown)), null)
     }
 
     override suspend fun createMenuItems(restaurantId: Any, menuItems: List<RestaurantMenuItemDTO>): Result<List<RestaurantMenuItemDTO>?> {
-        val res = api.post(menuItems, Endpoints.RESTAURANT_MENU_ITEMS(restaurantId.toString())) ?:
-        return Result(true, mapOf(pair= Pair("TOAST", R.string.error_connection_server)), null)
+        val res = api.post(menuItems, Endpoints.RESTAURANT_MENU_ITEMS(restaurantId.toString()))
 
-        if (res.status == HttpStatusCode.Created){
+        if(res.isError)
+            return Result(isError = true, errors = res.errors, value = null)
+
+        if (res.value!!.status == HttpStatusCode.Created){
             return try {
-                val j:List<RestaurantMenuItemDTO> = res.body()
-                Result(isError = false, value = j)
+                Result(isError = false, value =  res.value.body())
             }
             catch (e: Exception){
                 Result(isError = true, errors = mapOf(pair= Pair("TOAST", R.string.error_unknown)) ,value = null)
             }
         }
-
-        if (res.status == HttpStatusCode.Unauthorized)
-            return Result(isError = true, errors = mapOf(pair= Pair("TOAST", R.string.error_unauthorized_access)) ,value = null)
-
 
         return Result(true, mapOf(pair = Pair("TOAST", R.string.error_unknown)), null)
     }
 
 
     override suspend fun getMenuItems(restaurantId: Any): Result<List<RestaurantMenuItemDTO>?> {
-        val res = api.get(Endpoints.RESTAURANT_MENU_ITEMS(restaurantId.toString())) ?:
-        return Result(true, mapOf(pair= Pair("TOAST", R.string.error_connection_server)), null)
+        val res = api.get(Endpoints.RESTAURANT_MENU_ITEMS(restaurantId.toString()))
 
+        if(res.isError)
+            return Result(isError = true, errors = res.errors, value = null)
 
-        if (res.status == HttpStatusCode.OK){
+        if (res.value!!.status == HttpStatusCode.OK){
             return try {
-                val j:List<RestaurantMenuItemDTO> = res.body()
-                Result(isError = false, value = j)
+                Result(isError = false, value = res.value.body())
             }
             catch (e: Exception){
                 Result(isError = true, errors = mapOf(pair= Pair("TOAST", R.string.error_unknown)) ,value = null)
             }
         }
-
-        if (res.status == HttpStatusCode.Unauthorized)
-            return Result(isError = true, errors = mapOf(pair= Pair("TOAST", R.string.error_unauthorized_access)) ,value = null)
-
 
         return Result(true, mapOf(pair = Pair("TOAST", R.string.error_unknown)), null)
     }
 
     override suspend fun getMenuItem(restaurantId: Any, itemId: Any): Result<RestaurantMenuItemDTO?> {
-        val res = api.get(Endpoints.RESTAURANT_MENU_ITEM(restaurantId.toString(), itemId.toString())) ?:
-        return Result(true, mapOf(pair= Pair("TOAST", R.string.error_connection_server)), null)
+        val res = api.get(Endpoints.RESTAURANT_MENU_ITEM(restaurantId.toString(), itemId.toString()))
 
+        if(res.isError)
+            return Result(isError = true, errors = res.errors, value = null)
 
-        if (res.status == HttpStatusCode.OK){
+        if (res.value!!.status == HttpStatusCode.OK){
             return try {
-                val j:RestaurantMenuItemDTO = res.body()
-                Result(isError = false, value = j)
+                Result(isError = false, value = res.value.body())
             }
             catch (e: Exception){
                 Result(isError = true, errors = mapOf(pair= Pair("TOAST", R.string.error_unknown)) ,value = null)
             }
         }
-
-        if (res.status == HttpStatusCode.Unauthorized)
-            return Result(isError = true, errors = mapOf(pair= Pair("TOAST", R.string.error_unauthorized_access)) ,value = null)
-
 
         return Result(true, mapOf(pair = Pair("TOAST", R.string.error_unknown)), null)
     }
