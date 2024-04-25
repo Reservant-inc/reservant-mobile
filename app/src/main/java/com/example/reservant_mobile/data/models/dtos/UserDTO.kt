d45054dc4290a32b4c36e60340c247db86e9635e
Mroxny <50219282+Mroxny@users.noreply.github.com>
2024-04-25 22:39:55 +0200

    Make null default

--- app/src/main/java/com/example/reservant_mobile/data/models/dtos/UserDTO.kt
+++ app/src/main/java/com/example/reservant_mobile/data/models/dtos/UserDTO.kt
@@ -4,12 +4,12 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 class UserDTO (
-    val id: String = "",
+    val id: String? = null,
     val login: String,
     val firstName: String,
     val lastName: String,
-    val roles: List<String> = emptyList(),
-    val phoneNumber: String = "",
-    val employments: List<RestaurantEmployeeDTO> = emptyList(),
-    val token: String = ""
+    val roles: List<String>? = null,
+    val phoneNumber: String? = null,
+    val employments: List<RestaurantEmployeeDTO>? = null,
+    val token: String? = null
 )
