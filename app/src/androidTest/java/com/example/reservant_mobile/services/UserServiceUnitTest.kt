672ed720768adb721841f83ac38280e5b6f0a808
Mroxny <50219282+Mroxny@users.noreply.github.com>
2024-05-14 22:22:05 +0200

    Update UserServiceUnitTest.kt

--- app/src/androidTest/java/com/example/reservant_mobile/services/UserServiceUnitTest.kt
+++ app/src/androidTest/java/com/example/reservant_mobile/services/UserServiceUnitTest.kt
@@ -1,73 +1,41 @@
 package com.example.reservant_mobile.services
 
-import com.example.reservant_mobile.data.models.dtos.LoginCredentialsDTO
-import com.example.reservant_mobile.data.models.dtos.RegisterUserDTO
-import com.example.reservant_mobile.data.models.dtos.UserDTO
-import com.example.reservant_mobile.data.models.dtos.fields.Result
-import com.example.reservant_mobile.data.services.IUserService
-import com.example.reservant_mobile.data.services.LocalBearerService
-import com.example.reservant_mobile.data.services.UserService
 import com.google.common.truth.Truth.assertThat
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.runTest
-import org.junit.After
-import org.junit.Before
 import org.junit.Test
 
-class UserServiceUnitTest {
-    private val localBearer = LocalBearerService()
-    private val ser:IUserService = UserService()
+class UserServiceUnitTest: ServiceTest(){
 
-    private lateinit var loginUser: LoginCredentialsDTO
-    private lateinit var existingToken: String
-
-
-    @Before
-    fun setupData() = runBlocking {
-        existingToken =  localBearer.getBearerToken()
-        loginUser= LoginCredentialsDTO(
-            login = "JD",
-            password = "Pa${"$"}${"$"}w0rd",
-            rememberMe = false
-        )
-    }
     @Test
     fun non_unique_login_returns_false() = runTest {
-        assertThat(ser.isLoginUnique("JD")).isFalse()
+        assertThat(userService.isLoginUnique("JD")).isFalse()
     }
 
     @Test
     fun valid_login_returns_no_error() = runTest {
-        assertThat(ser.loginUser(loginUser).isError).isFalse()
+        assertThat(userService.loginUser(loginUser).isError).isFalse()
     }
 
     @Test
     fun refresh_token_returns_true() = runTest {
-        ser.logoutUser()
-        ser.loginUser(loginUser)
-        assertThat(ser.refreshToken()).isTrue()
+        userService.logoutUser()
+        userService.loginUser(loginUser)
+        assertThat(userService.refreshToken()).isTrue()
     }
 
     @Test
     fun logout_make_empty_token() = runTest {
         localBearer.saveBearerToken("test")
-        ser.logoutUser()
+        userService.logoutUser()
         val token = localBearer.getBearerToken()
         assertThat(token).isEmpty()
     }
 
     @Test
     fun logout_clears_cache() = runTest {
-        ser.loginUser(loginUser)
-        assertThat(ser.refreshToken()).isTrue()
-        ser.logoutUser()
-        assertThat(ser.refreshToken()).isFalse()
-    }
-
-    @After
-    fun cleanPostTestData() = runBlocking{
-        if(existingToken.isNotEmpty())
-            localBearer.saveBearerToken(existingToken)
+        userService.loginUser(loginUser)
+        assertThat(userService.refreshToken()).isTrue()
+        userService.logoutUser()
+        assertThat(userService.refreshToken()).isFalse()
     }
 }
