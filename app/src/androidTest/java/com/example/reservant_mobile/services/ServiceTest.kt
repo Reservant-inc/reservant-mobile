04fcc5534130c98b88c495f21271e77ee83767b1
Mroxny <50219282+Mroxny@users.noreply.github.com>
2024-05-14 22:22:09 +0200

    Create ServiceTest.kt

---
+++ app/src/androidTest/java/com/example/reservant_mobile/services/ServiceTest.kt
@@ -0,0 +1,37 @@
+package com.example.reservant_mobile.services
+
+import com.example.reservant_mobile.data.models.dtos.LoginCredentialsDTO
+import com.example.reservant_mobile.data.services.IUserService
+import com.example.reservant_mobile.data.services.LocalBearerService
+import com.example.reservant_mobile.data.services.UserService
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Before
+
+abstract class ServiceTest {
+    private lateinit var existingToken: String
+    protected val localBearer = LocalBearerService()
+    protected val userService: IUserService = UserService()
+    protected var loginUser: LoginCredentialsDTO = LoginCredentialsDTO(
+                                                    login = "JD",
+                                                    password = "Pa${"$"}${"$"}w0rd",
+                                                    rememberMe = false
+                                                    )
+    @Before
+    fun getPreTestData() = runBlocking {
+        existingToken = localBearer.getBearerToken()
+    }
+
+    @After
+    fun cleanPostTestData() = runBlocking{
+        userService.logoutUser()
+        if(existingToken.isNotEmpty())
+            localBearer.saveBearerToken(existingToken)
+    }
+
+    suspend fun loginUser(){
+        userService.loginUser(loginUser)
+    }
+
+
+}
