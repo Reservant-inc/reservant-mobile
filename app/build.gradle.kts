d47c797a7924ce2433124a196ad48403d34cad00
Aleksander Babij <aleksander.babij@gmail.com>
2024-05-17 16:56:43 +0200

    added dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -86,7 +86,7 @@ dependencies {
     implementation("io.ktor:ktor-client-auth:$ktorVersion")
     implementation("io.ktor:ktor-client-resources:$ktorVersion")
 
-    implementation("androidx.navigation:navigation-compose:2.5.1")
+    implementation("androidx.navigation:navigation-compose:2.8.0-beta01")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
     implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.0")
     implementation ("androidx.compose.material:material-icons-extended:1.6.4")
