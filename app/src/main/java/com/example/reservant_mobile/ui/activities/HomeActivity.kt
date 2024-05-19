0aa5ccb64047261ca542e62466803ed19908de2b
Aleksander Babij <aleksander.babij@gmail.com>
2024-05-19 16:05:40 +0200

    added padding

--- app/src/main/java/com/example/reservant_mobile/ui/activities/HomeActivity.kt
+++ app/src/main/java/com/example/reservant_mobile/ui/activities/HomeActivity.kt
@@ -1,19 +1,19 @@
 package com.example.reservant_mobile.ui.activities
 
-import EmployeeManagementActivity
 import android.annotation.SuppressLint
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.reservant_mobile.ui.components.BottomNavigation
 import com.example.reservant_mobile.ui.components.Content
 import com.example.reservant_mobile.ui.constants.MainRoutes
-import com.example.reservant_mobile.ui.constants.Management
 import com.example.reservant_mobile.ui.constants.RegisterRestaurantRoutes
 import com.example.reservant_mobile.ui.constants.RestaurantManagementRoutes
 import com.example.reservant_mobile.ui.theme.AppTheme
@@ -35,7 +35,7 @@ fun HomeActivity() {
                 BottomNavigation(innerNavController)
             }
         ){
-            NavHost(navController = innerNavController, startDestination = MainRoutes.ACTIVITY_HOME){
+            NavHost(navController = innerNavController, startDestination = MainRoutes.ACTIVITY_HOME, modifier = Modifier.padding(it)){
                 composable(MainRoutes.ACTIVITY_HOME){
                     Content()
                 }
