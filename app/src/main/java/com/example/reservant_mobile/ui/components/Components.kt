1ad9ede14c6ad4ccdf85a8c8da3a9631f1af4e26
Aleksander Babij <aleksander.babij@gmail.com>
2024-05-27 19:23:11 +0200

    added logic for applying modifier

--- app/src/main/java/com/example/reservant_mobile/ui/components/Components.kt
+++ app/src/main/java/com/example/reservant_mobile/ui/components/Components.kt
@@ -15,6 +15,7 @@ import androidx.compose.animation.core.updateTransition
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.basicMarquee
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
@@ -81,6 +82,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.CircularProgressIndicator
@@ -107,6 +109,8 @@ import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.SelectableDates
+import androidx.compose.material3.ShapeDefaults
+import androidx.compose.material3.Shapes
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabPosition
 import androidx.compose.material3.TabRow
@@ -127,12 +131,17 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.blur
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.paint
 import androidx.compose.ui.draw.scale
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.onGloballyPositioned
@@ -1619,13 +1628,24 @@ fun MenuCard(
         }
     }
 
+    val loading by remember {
+        mutableStateOf(true)
+    }
+
+    val loadingModifier = when {
+        loading ->  Modifier
+            .background(Color.Gray, ShapeDefaults.Medium)
+            .alpha(0F)
+        else -> Modifier
+    }
 
     Card(
+        elevation = CardDefaults.cardElevation(8.dp),
         modifier = Modifier
             .fillMaxWidth()
             .padding(8.dp)
-            .clickable(onClick = onClick),
-        elevation = CardDefaults.cardElevation(8.dp)
+            .clickable(onClick = onClick)
+            .then(loadingModifier)
     ) {
         Column {
             Image(
@@ -1978,7 +1998,12 @@ fun MyFloatingActionButton(
         onClick = onClick,
         modifier = Modifier
             .padding(allPadding)
-            .padding(top = topPadding, bottom = bottomPadding, start = startPadding, end = endPadding),
+            .padding(
+                top = topPadding,
+                bottom = bottomPadding,
+                start = startPadding,
+                end = endPadding
+            ),
         content = {
             Icon(
                 imageVector = Icons.Default.Add,
