034b0fd2b16c40fc30699f7043aa4df3be07ef73
Dzwiedzi <108011192+Dzwiedzi@users.noreply.github.com>
2024-06-12 00:58:45 +0200

    added support for letters different than latin (ą,ś,ć,ó...)

--- app/src/main/java/com/example/reservant_mobile/data/constants/Regex.kt
+++ app/src/main/java/com/example/reservant_mobile/data/constants/Regex.kt
@@ -2,7 +2,7 @@ package com.example.reservant_mobile.data.constants
 
 data object Regex{
     const val LOGIN = "[\\w.\\-$!]{2,}"
-    const val NAME_REG = "^[A-Za-z]+$"
+    const val NAME_REG = "^[\\p{L}]+$" //litery specjalne z różnych języków
     const val DATE_REG = "^[0,1,2]\\d{3}-[0,1]\\d-[0,1,2]\\d\$"
     const val EMAIL_REG = "[a-zA-z0-9][\\w.-]*@[a-zA-z0-9]{2,}\\.[a-zA-Z]{2,}"
     const val PHONE_REG = "^\\d{9,15}$" // 9-15 digits
