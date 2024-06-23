749b02b9a0d573994243f5957160ad9093d8198d
Mroxny <50219282+Mroxny@users.noreply.github.com>
2024-06-23 15:51:16 +0200

    Add new restaurant endpoints

---
+++ app/src/main/java/com/example/reservant_mobile/data/endpoints/Restaurants.kt
@@ -0,0 +1,18 @@
+package com.example.reservant_mobile.data.endpoints
+
+import io.ktor.resources.Resource
+
+@Resource("/restaurants")
+class Restaurants {
+    @Resource("in-area")
+    class InArea(val parent: Restaurants = Restaurants(), val lat1: Double, val lon1: Double, val lat2: Double, val lon2: Double)
+    @Resource("{restaurantId}")
+    class Id(val parent: MyRestaurants = MyRestaurants(), val id: String){
+        @Resource("orders")
+        class Orders(val parent: Id)
+        @Resource("events")
+        class Events(val parent: Id)
+        @Resource("reviews")
+        class Reviews(val parent: Id)
+    }
+}
