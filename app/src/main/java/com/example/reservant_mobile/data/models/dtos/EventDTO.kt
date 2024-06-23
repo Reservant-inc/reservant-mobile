640c79d399c26b7582e674206f05202cebddb9b4
Mroxny <50219282+Mroxny@users.noreply.github.com>
2024-06-23 18:09:42 +0200

    Add event service

---
+++ app/src/main/java/com/example/reservant_mobile/data/models/dtos/EventDTO.kt
@@ -0,0 +1,21 @@
+package com.example.reservant_mobile.data.models.dtos
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class EventDTO (
+    val eventId: Int? = null ,
+    val createdAt: String? = null,
+    val description: String,
+    val time: String,
+    val mustJoinUntil: String,
+    val creatorId: String? = null,
+    val creatorFullName: String? = null,
+    val restaurantId: Int,
+    val restaurantName:String? = null,
+    val visitId: Int? = null,
+    /***
+     * Interested user contains only userId, firstname, lastname
+     */
+    val interested: List<UserDTO>? = null
+)
