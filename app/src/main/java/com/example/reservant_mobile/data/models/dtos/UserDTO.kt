1d57a4a2586f6002ced726f2642384d36eaeeedf
Mroxny <50219282+Mroxny@users.noreply.github.com>
2024-06-23 18:10:10 +0200

    Update UserDTO

--- app/src/main/java/com/example/reservant_mobile/data/models/dtos/UserDTO.kt
+++ app/src/main/java/com/example/reservant_mobile/data/models/dtos/UserDTO.kt
@@ -5,7 +5,7 @@ import kotlinx.serialization.Serializable
 @Serializable
 class UserDTO (
     val userId: String? = null,
-    val login: String,
+    val login: String? = null,
     val firstName: String,
     val lastName: String,
     val roles: List<String>? = null,
