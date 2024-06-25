ed47619e1f6eee56b4ea3e57b30d9485500b15f9
Dzwiedzi <108011192+Dzwiedzi@users.noreply.github.com>
2024-06-25 18:33:37 +0200

    settings activity

--- app/src/main/java/com/example/reservant_mobile/ui/activities/RestaurantOwnerProfileActivity.kt
+++ app/src/main/java/com/example/reservant_mobile/ui/activities/RestaurantOwnerProfileActivity.kt
@@ -1,14 +1,14 @@
 package com.example.reservant_mobile.ui.activities
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material3.Surface
+import com.example.reservant_mobile.ui.components.SettingItem
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ExitToApp
+import androidx.compose.material.icons.automirrored.filled.Help
+import androidx.compose.material.icons.filled.*
+import androidx.compose.material.icons.rounded.RestaurantMenu
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -16,69 +16,95 @@ import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.example.reservant_mobile.R
-import com.example.reservant_mobile.data.constants.Roles
-import com.example.reservant_mobile.data.services.UserService
-import com.example.reservant_mobile.ui.components.ButtonComponent
+import com.example.reservant_mobile.ui.components.IconWithHeader
 import com.example.reservant_mobile.ui.navigation.AuthRoutes
-import com.example.reservant_mobile.ui.navigation.MainRoutes
-import com.example.reservant_mobile.ui.navigation.RegisterRestaurantRoutes
-import com.example.reservant_mobile.ui.navigation.RestaurantRoutes
 import com.example.reservant_mobile.ui.viewmodels.LoginViewModel
 import kotlinx.coroutines.launch
 
 @Composable
-fun RestaurantOwnerProfileActivity(navController: NavController, themeChange: () -> Unit){
-
+fun RestaurantOwnerProfileActivity(navController: NavController, themeChange: () -> Unit) {
     val loginViewModel = viewModel<LoginViewModel>()
 
     Surface {
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(16.dp),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
+                .padding(16.dp)
         ) {
+            Spacer(modifier = Modifier.padding(top = 8.dp))
 
-            var newRestaurantLabel:Int = R.string.label_become_restaurant_owner
-            if(Roles.RESTAURANT_OWNER in UserService.User.roles)
-                newRestaurantLabel = R.string.label_register_restaurant
-
-            ButtonComponent(
-                modifier = Modifier
-                    .wrapContentSize()
-                    .padding(16.dp),
-                label = stringResource(id = newRestaurantLabel),
-                onClick = {
-                    navController.navigate(RegisterRestaurantRoutes.Register)
-                },
+            IconWithHeader(
+                icon = Icons.Rounded.RestaurantMenu,
+                text = stringResource(R.string.label_settings),
             )
 
-            ButtonComponent(
-                label = "Temporary theme changer",
-                onClick = { themeChange() }
+            Spacer(modifier = Modifier.padding(top = 16.dp))
+
+            SettingItem(
+                icon = Icons.Filled.Person,
+                text = stringResource(id = R.string.label_edit_profile),
+                onClick = { /* Navigate to Edit Profile */ }
             )
 
-            ButtonComponent(
-                modifier = Modifier
-                    .wrapContentSize()
-                    .padding(16.dp),
-                label = "Restaurant Detail Preview",
-                onClick = {
-                    navController.navigate(RestaurantRoutes.Details(restaurantId = 1))
-                },
+            SettingItem(
+                icon = Icons.Filled.AccountBalanceWallet,
+                text = stringResource(id = R.string.label_wallet),
+                onClick = { /* Navigate to Wallet */ }
             )
 
-            ButtonComponent(
-                label = stringResource(id = R.string.label_logout_action),
+            SettingItem(
+                icon = Icons.Filled.ShoppingCart,
+                text = stringResource(id = R.string.label_my_orders),
+                onClick = { /* Navigate to My Orders */ }
+            )
+
+            SettingItem(
+                icon = Icons.AutoMirrored.Filled.Help,
+                text = stringResource(id = R.string.label_helpdesk),
+                onClick = { /* Navigate to Helpdesk */ }
+            )
+
+            SettingItem(
+                icon = Icons.Filled.Info,
+                text = stringResource(id = R.string.label_faq),
+                onClick = { /* Navigate to FAQ */ }
+            )
+
+            SettingItem(
+                icon = Icons.Filled.CardGiftcard,
+                text = stringResource(id = R.string.label_promo_codes),
+                onClick = { /* Navigate to Promo Codes */ }
+            )
+
+            SettingItem(
+                icon = Icons.Filled.Settings,
+                text = stringResource(id = R.string.label_app_settings),
+                onClick = { /* Navigate to App Settings */ }
+            )
+
+            SettingItem(
+                icon = Icons.Filled.Delete,
+                text = stringResource(id = R.string.label_delete_account),
+                onClick = { /* Navigate to Delete Account */ }
+            )
+
+            SettingItem(
+                icon = Icons.AutoMirrored.Filled.ExitToApp,
+                text = stringResource(id = R.string.label_logout_action),
                 onClick = {
-                    loginViewModel.viewModelScope.launch{
+                    loginViewModel.viewModelScope.launch {
                         loginViewModel.logout()
-                        navController.navigate(AuthRoutes.Landing){
+                        navController.navigate(AuthRoutes.Landing) {
                             popUpTo(0)
                         }
                     }
                 }
+            )
+
+            SettingItem(
+                icon = Icons.Filled.Brightness4,
+                text = "Temporary theme changer",
+                onClick = { themeChange() }
             )
         }
     }
