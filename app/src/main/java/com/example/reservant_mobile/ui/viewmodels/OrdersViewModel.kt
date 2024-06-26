59ed6b7a954ec29e2985f7644b7b0d5826992fb6
Aleksander Babij <aleksander.babij@gmail.com>
2024-06-26 17:54:40 +0200

    made fetchorders suspend

--- app/src/main/java/com/example/reservant_mobile/ui/viewmodels/OrdersViewModel.kt
+++ app/src/main/java/com/example/reservant_mobile/ui/viewmodels/OrdersViewModel.kt
@@ -18,27 +18,25 @@ class OrdersViewModel : ViewModel() {
     private var allOrders: List<OrderDTO> = emptyList()
 
     init {
-        fetchOrders()
+        viewModelScope.launch { fetchOrders() }
     }
 
-    private fun fetchOrders() {
-        viewModelScope.launch {
-            val fetchedOrders = listOf(
-                OrderDTO("10-05-2024", "90 PLN", "Jan Kowalski", "Odebrano"),
-                OrderDTO("11-05-2024", "150 PLN", "Anna Nowak", "Anulowano"),
-                OrderDTO("12-05-2024", "200 PLN", "Piotr Wiśniewski", "Odebrano"),
-                OrderDTO("13-05-2024", "120 PLN", "Katarzyna Zielińska", "Odebrano"),
-                OrderDTO("14-05-2024", "130 PLN", "Marcin Lewandowski", "Anulowano"),
-                OrderDTO("15-05-2024", "80 PLN", "Sylwia Wójcik", "Odebrano"),
-                OrderDTO("16-05-2024", "170 PLN", "Krzysztof Krawczyk", "Odebrano"),
-                OrderDTO("17-05-2024", "110 PLN", "Magdalena Kaczmarek", "Anulowano"),
-                OrderDTO("18-05-2024", "140 PLN", "Rafał Piotrowski", "Odebrano"),
-                OrderDTO("19-05-2024", "190 PLN", "Agnieszka Kowal", "Odebrano")
-            )
-            allOrders = fetchedOrders
-            _orders.value = fetchedOrders
-            _filteredOrders.value = fetchedOrders
-        }
+    private suspend fun fetchOrders() {
+        val fetchedOrders = listOf(
+            OrderDTO("10-05-2024", "90 PLN", "Jan Kowalski", "Odebrano"),
+            OrderDTO("11-05-2024", "150 PLN", "Anna Nowak", "Anulowano"),
+            OrderDTO("12-05-2024", "200 PLN", "Piotr Wiśniewski", "Odebrano"),
+            OrderDTO("13-05-2024", "120 PLN", "Katarzyna Zielińska", "Odebrano"),
+            OrderDTO("14-05-2024", "130 PLN", "Marcin Lewandowski", "Anulowano"),
+            OrderDTO("15-05-2024", "80 PLN", "Sylwia Wójcik", "Odebrano"),
+            OrderDTO("16-05-2024", "170 PLN", "Krzysztof Krawczyk", "Odebrano"),
+            OrderDTO("17-05-2024", "110 PLN", "Magdalena Kaczmarek", "Anulowano"),
+            OrderDTO("18-05-2024", "140 PLN", "Rafał Piotrowski", "Odebrano"),
+            OrderDTO("19-05-2024", "190 PLN", "Agnieszka Kowal", "Odebrano")
+        )
+        allOrders = fetchedOrders
+        _orders.value = fetchedOrders
+        _filteredOrders.value = fetchedOrders
     }
 
     fun searchOrders(query: String) {
