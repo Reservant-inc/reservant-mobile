261850bbf955fc2a3687d83723e2fe9e2d7e4fc0
Maciej Mrozek <50219282+Mroxny@users.noreply.github.com>
2024-07-14 13:45:57 +0200

    Create OrdersServiceUnitTest.kt

---
+++ app/src/androidTest/java/reservant_mobile/services/OrdersServiceUnitTest.kt
@@ -0,0 +1,57 @@
+package reservant_mobile.services
+
+import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+import reservant_mobile.data.models.dtos.OrderDTO
+import reservant_mobile.data.services.IOrdersService
+import reservant_mobile.data.services.OrdersService
+
+class OrdersServiceUnitTest: ServiceTest(){
+    private val ser:IOrdersService = OrdersService()
+
+    private lateinit var order:OrderDTO
+    private var orderId:Int = 1
+
+
+    @Before
+    fun setupData() = runBlocking {
+        loginUser()
+
+        order = OrderDTO(
+            visitId = 1,
+            note = "Test note",
+            items = listOf(
+                OrderDTO.OrderItemDTO(
+                menuItemId = 1,
+                amount = 1),
+                OrderDTO.OrderItemDTO(
+                    menuItemId = 1,
+                    amount = 2),
+            )
+        )
+    }
+
+    @Test
+    fun get_order_return_not_null()= runTest{
+        assertThat(ser.getOrder(orderId).value).isNotNull()
+    }
+
+    @Test
+    fun cancel_order_return_true()= runTest{
+        assertThat(ser.cancelOrder(orderId).value).isNotNull()
+    }
+
+    @Test
+    fun create_order_return_not_null()= runTest{
+        assertThat(ser.createOrder(order).value).isNotNull()
+    }
+
+//    todo: need to wait for proper emp implementation
+//    @Test
+//    fun set_order_status_return_not_null()= runTest{
+//        assertThat(ser.changeOrderStatus(orderId, order).value).isNotNull()
+//    }
+}
