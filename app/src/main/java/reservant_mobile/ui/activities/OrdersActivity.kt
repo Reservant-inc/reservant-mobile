e1350225151b64b7854d4045d8f3a5a943d4c035
Dzwiedzi <108011192+Dzwiedzi@users.noreply.github.com>
2024-07-24 02:03:21 +0200

    fix

--- app/src/main/java/reservant_mobile/ui/activities/OrdersActivity.kt
+++ app/src/main/java/reservant_mobile/ui/activities/OrdersActivity.kt
@@ -1,17 +1,12 @@
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.rounded.History
 import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -23,8 +18,6 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.reservant_mobile.R
-import reservant_mobile.ui.components.ButtonComponent
-import reservant_mobile.ui.components.FilterDialog
 import reservant_mobile.ui.components.IconWithHeader
 import reservant_mobile.ui.components.OrderItem
 import reservant_mobile.ui.components.SearchBarWithFilter
