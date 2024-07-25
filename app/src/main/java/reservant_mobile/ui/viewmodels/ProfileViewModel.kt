0d4dbe2b2d146bac97112aa5c2656861b0d7e0e8
Michał Wiśniewski <s24845@pjwstk.edu.pl>
2024-07-25 10:48:34 +0200

    Created ProfileViewModel

--- app/src/main/java/reservant_mobile/ui/viewmodels/ProfileViewModel.kt
+++ app/src/main/java/reservant_mobile/ui/viewmodels/ProfileViewModel.kt
@@ -1,6 +1,13 @@
 package reservant_mobile.ui.viewmodels
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import reservant_mobile.data.models.dtos.LoggedUserDTO
+import reservant_mobile.data.models.dtos.UserDTO
 import reservant_mobile.data.services.IUserService
 import reservant_mobile.data.services.UserService
 
@@ -8,13 +15,21 @@ class ProfileViewModel(
     private val userService: IUserService = UserService()
 ) : ViewModel() {
 
+    var user: LoggedUserDTO? by mutableStateOf(null)
+
     init {
-
-        user = userService.getUser()
-
-
-
+        viewModelScope.launch {
+            loadUser()
+        }
     }
 
+    private suspend fun loadUser(): Boolean {
+        val resultUser =  userService.getUser()
+        if(resultUser.isError){
+            return false
+        }
+        user = resultUser.value
+        return true
+    }
 
 }
