25c1ea9724f9a63d0a43611692cca19d917bb6d3
Michał Wiśniewski <s24845@pjwstk.edu.pl>
2024-07-26 15:23:54 +0200

    Removed unused imports

--- app/src/main/java/reservant_mobile/ui/activities/RestaurantReservationActivity.kt
+++ app/src/main/java/reservant_mobile/ui/activities/RestaurantReservationActivity.kt
@@ -12,15 +12,12 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
