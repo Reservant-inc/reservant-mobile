a3f4eb7b594652e50e3c2d681591d6ef27f72e26
Michał Wiśniewski <s24845@pjwstk.edu.pl>
2024-07-26 14:49:33 +0200

    Renamed Activity

---
+++ app/src/main/java/reservant_mobile/ui/activities/RestaurantReservationActivity.kt
@@ -0,0 +1,113 @@
+package reservant_mobile.ui.activities
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.stringResource
+import com.example.reservant_mobile.R
+import reservant_mobile.ui.components.DeliveryContent
+import reservant_mobile.ui.components.DineInContent
+import reservant_mobile.ui.components.FloatingTabSwitch
+import reservant_mobile.ui.components.TakeawayContent
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun RestaurantReservationActivity(navController: NavHostController) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(text = "Rezerwacja stolika", fontWeight = FontWeight.Bold)
+                    }
+                },
+                navigationIcon = {
+                    IconButton(onClick = { navController.popBackStack() }) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = stringResource(R.string.label_back)
+                        )
+                    }
+                }
+            )
+        }
+    ) { paddingValues ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                contentAlignment = Alignment.TopEnd
+            ) {
+                Box(
+                    modifier = Modifier
+                        .height(680.dp)
+                        .width(360.dp)
+                        .clip(RoundedCornerShape(16.dp))
+                        .background(MaterialTheme.colorScheme.surface)
+                        .padding(start = 16.dp, end = 16.dp, bottom = 8.dp)
+                        .verticalScroll(rememberScrollState())
+                ) {
+                    FloatingTabSwitch(
+                        pages = listOf(
+                            "Na miejscu" to {
+                                DineInContent(
+                                    onDineInClick = { /* Handle dine-in click */ },
+                                    modifier = Modifier.padding(top = 88.dp)
+                                )
+                            },
+                            "Dostawa" to {
+                                DeliveryContent(
+                                    onDeliveryClick = { /* Handle delivery click */ },
+                                    modifier = Modifier.padding(top = 88.dp)
+                                )
+                            },
+                            "Odbiór" to {
+                                TakeawayContent(
+                                    onTakeawayClick = { /* Handle takeaway click */ },
+                                    modifier = Modifier.padding(top = 88.dp)
+                                )
+                            }
+                        ),
+                        paneScroll = false
+                    )
+                }
+            }
+        }
+    }
+}
