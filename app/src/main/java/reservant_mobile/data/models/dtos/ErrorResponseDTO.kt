5ebf12147fa84882bfa4bae1df63cd0d25d809fb
Maciej Mrozek <50219282+Mroxny@users.noreply.github.com>
2024-08-09 14:17:20 +0200

    Fix DTO 2

--- app/src/main/java/reservant_mobile/data/models/dtos/ErrorResponseDTO.kt
+++ app/src/main/java/reservant_mobile/data/models/dtos/ErrorResponseDTO.kt
@@ -5,6 +5,6 @@ import kotlinx.serialization.Serializable
 @Serializable
 class ErrorResponseDTO (
     val status: Int?,
-    val errors: Map<String, List<String>?>,
-    val errorCodes: Map<String, List<String>?>
+    val errors: Map<String, List<String?>>,
+    val errorCodes: Map<String, List<String?>>
 )
