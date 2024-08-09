876c1ba62c426def094cde961b8191c5ddf598a1
Maciej Mrozek <50219282+Mroxny@users.noreply.github.com>
2024-08-09 13:45:38 +0200

    Implement ServiceUtil

--- app/src/main/java/reservant_mobile/data/services/RestaurantMenuService.kt
+++ app/src/main/java/reservant_mobile/data/services/RestaurantMenuService.kt
@@ -1,8 +1,5 @@
 package reservant_mobile.data.services
 
-import com.example.reservant_mobile.R
-import io.ktor.client.call.body
-import io.ktor.client.statement.HttpResponse
 import io.ktor.http.HttpStatusCode
 import reservant_mobile.data.endpoints.MenuItems
 import reservant_mobile.data.endpoints.Menus
@@ -26,94 +23,61 @@ interface IRestaurantMenuService{
     suspend fun deleteMenuItem(id: Any): Result<Boolean>
 }
 
-class RestaurantMenuService(private var api: APIService = APIService()): IRestaurantMenuService {
-    private suspend inline fun <reified T> resultWrapper(res: Result<HttpResponse?>, expectedCode:HttpStatusCode = HttpStatusCode.OK): Result<T?> {
-        if(res.isError)
-            return Result(isError = true, errors = res.errors, value = null)
-
-        if (res.value!!.status == expectedCode){
-            return try {
-                val r:T = res.value.body()
-                Result(isError = false, value = r)
-            }
-            catch (e: Exception){
-                println("SERVICE ERROR: $e")
-                Result(isError = true, errors = mapOf(pair= Pair("TOAST", R.string.error_unknown)) ,value = null)
-            }
-        }
-
-        return Result(true, mapOf(pair = Pair("TOAST", R.string.error_unknown)), null)
-    }
+class RestaurantMenuService():ServiceUtil(), IRestaurantMenuService {
 
     override suspend fun addMenu(menu: RestaurantMenuDTO): Result<RestaurantMenuDTO?> {
         val res = api.post(Menus(), menu)
-        return resultWrapper(res)
+        return complexResultWrapper(res)
     }
 
     override suspend fun getMenus(restaurantId: Any): Result<List<RestaurantMenuDTO>?> {
         val res = api.get(MyRestaurants.Id.Menus(MyRestaurants.Id(id=restaurantId.toString())))
-        return resultWrapper(res)
+        return complexResultWrapper(res)
     }
 
     override suspend fun getMenu(id: Any): Result<RestaurantMenuDTO?> {
         val res = api.get(Menus.Id(id=id.toString()))
-        return resultWrapper(res)
+        return complexResultWrapper(res)
     }
 
     override suspend fun editMenu(id: Any, menu: RestaurantMenuDTO): Result<RestaurantMenuDTO?> {
         val res = api.put(Menus.Id(id=id.toString()), menu)
-        return resultWrapper(res)
+        return complexResultWrapper(res)
     }
 
     override suspend fun deleteMenu(id: Any): Result<Boolean> {
         val res = api.delete(Menus.Id(id=id.toString()))
-        
-        if(res.isError)
-            return Result(isError = true, errors = res.errors, value = false)
-
-        if (res.value!!.status == HttpStatusCode.NoContent)
-            return Result(isError = false, value = true)
-
-        return Result(true, mapOf(pair = Pair("TOAST", R.string.error_unknown)), false)
+        return booleanResultWrapper(res, expectedCode = HttpStatusCode.NoContent)
     }
 
     override suspend fun addItemsToMenu(menuId: Any, itemsIds:List<Int>): Result<RestaurantMenuDTO?> {
         val newGroup: HashMap<String, List<Int>> = hashMapOf("itemIds" to itemsIds)
         val res = api.post(Menus.Id.Items(Menus.Id(id=menuId.toString())), newGroup)
-        return resultWrapper(res)
-
+        return complexResultWrapper(res)
     }
 
     override suspend fun createMenuItem(menuItems: RestaurantMenuItemDTO): Result<RestaurantMenuItemDTO?> {
         val res = api.post( MenuItems(), menuItems)
-        return resultWrapper(res, HttpStatusCode.Created)
+        return complexResultWrapper(res, HttpStatusCode.Created)
     }
-
 
     override suspend fun getMenuItems(restaurantId: Any): Result<List<RestaurantMenuItemDTO>?> {
         val res = api.get(MyRestaurants.Id.MenuItems(MyRestaurants.Id(id=restaurantId.toString())))
-        return resultWrapper(res)
+        return complexResultWrapper(res)
     }
 
     override suspend fun getMenuItem(id: Any): Result<RestaurantMenuItemDTO?> {
         val res = api.get(MenuItems.Id(id=id.toString()))
-        return resultWrapper(res)
+        return complexResultWrapper(res)
     }
 
     override suspend fun editMenuItem(menuItemId: Any, item: RestaurantMenuItemDTO): Result<RestaurantMenuItemDTO?> {
         val res = api.put(MenuItems.Id(id=menuItemId.toString()), item)
-        return resultWrapper(res)
-
+        return complexResultWrapper(res)
     }
 
     override suspend fun deleteMenuItem(id: Any): Result<Boolean> {
         val res = api.delete(MenuItems.Id(id=id.toString()))
-
-        if(res.isError)
-            return Result(isError = true, errors = res.errors, value = false)
-        println("TEST"+res.value!!.status)
-        if (res.value!!.status == HttpStatusCode.NoContent)
-            return Result(isError = false, value = true)
-
-        return Result(true, mapOf(pair = Pair("TOAST", R.string.error_unknown)), false)    }
+        return booleanResultWrapper(res, expectedCode = HttpStatusCode.NoContent)
+    }
 }
