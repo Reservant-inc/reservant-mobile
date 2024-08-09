980bcd3c2a2e2d1c0260fd6105c192d5f0291f2b
Maciej Mrozek <50219282+Mroxny@users.noreply.github.com>
2024-08-09 22:56:18 +0200

    Update UserDTO.kt

--- app/src/main/java/reservant_mobile/data/models/dtos/UserDTO.kt
+++ app/src/main/java/reservant_mobile/data/models/dtos/UserDTO.kt
@@ -6,10 +6,20 @@ import kotlinx.serialization.Serializable
 class UserDTO (
     val userId: String? = null,
     val login: String? = null,
+    val email: String? = null,
+    val phoneNumber: String? = null,
     val firstName: String,
     val lastName: String,
+    /***
+     * Date in 'yyyy-MM-dd'T'HH:mm:ss.yyyy'Z'' format
+     */
+    val registeredAt: String? = null,
+    /***
+     * Date in 'yyyy-MM-dd' format
+     */
+    val birthDate: String? = null,
     val roles: List<String>? = null,
-    val phoneNumber: String? = null,
     val employments: List<RestaurantEmployeeDTO>? = null,
-    val token: String? = null
+    val token: String? = null,
+    val photo: String? = null
 )
