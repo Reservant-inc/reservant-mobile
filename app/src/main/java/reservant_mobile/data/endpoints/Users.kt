4b4b538a266fcdec2a3b356e20f676310ce621ba
Maciej Mrozek <50219282+Mroxny@users.noreply.github.com>
2024-08-10 09:40:53 +0200

    Update Users.kt

--- app/src/main/java/reservant_mobile/data/endpoints/Users.kt
+++ app/src/main/java/reservant_mobile/data/endpoints/Users.kt
@@ -3,7 +3,7 @@ package reservant_mobile.data.endpoints
 import io.ktor.resources.Resource
 
 @Resource("/users")
-class Users {
+class Users(val name: String? = null, val page: Int? = null, val perPage: Int? = null) {
 
     @Resource("{employeeId}")
     class Id(val parent: Users = Users(), val employeeId: String)
