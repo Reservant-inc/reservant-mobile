747f60f42e0753fbdcafdddda06c6393a1ec7599
Maciej Mrozek <50219282+Mroxny@users.noreply.github.com>
2024-08-11 20:27:15 +0200

    Add endpoints

---
+++ app/src/main/java/reservant_mobile/data/endpoints/Threads.kt
@@ -0,0 +1,17 @@
+package reservant_mobile.data.endpoints
+
+import io.ktor.resources.Resource
+
+@Resource("/threads")
+class Threads {
+    @Resource("{threadId}")
+    class ThreadId(val parent: Threads = Threads(), val threadId: String) {
+        @Resource("messages")
+        class Messages(
+            val parent: ThreadId,
+            val returnBefore: String? = null,
+            val page: Int? = null,
+            val perPage: Int? = null
+        )
+    }
+}
