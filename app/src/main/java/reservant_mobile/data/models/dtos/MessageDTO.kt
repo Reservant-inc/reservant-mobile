88bfc8d41c735f1dcf05ece78475e930c04e5ba9
Maciej Mrozek <50219282+Mroxny@users.noreply.github.com>
2024-08-11 20:37:09 +0200

    Add DTOs

---
+++ app/src/main/java/reservant_mobile/data/models/dtos/MessageDTO.kt
@@ -0,0 +1,20 @@
+package reservant_mobile.data.models.dtos
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class MessageDTO (
+    val messageId: Int? = null,
+    val contents: String,
+    /***
+     * Date in 'yyyy-MM-dd'T'HH:mm:ss.yyyy'Z'' format
+     */
+    val dateSent: String? = null,
+    /***
+     * Date in 'yyyy-MM-dd'T'HH:mm:ss.yyyy'Z'' format
+     */
+    val dateRead: String? = null,
+    val authorsFirstName: String? = null,
+    val authorsLastName: String? = null ,
+    val messageThreadId: Int? = null
+)
