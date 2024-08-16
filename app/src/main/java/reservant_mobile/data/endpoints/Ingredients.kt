73cd8215ad449734fb2f3b7e9d1c6ef437752264
Maciej Mrozek <50219282+Mroxny@users.noreply.github.com>
2024-08-16 13:40:44 +0200

    Update ingredients

---
+++ app/src/main/java/reservant_mobile/data/endpoints/Ingredients.kt
@@ -0,0 +1,6 @@
+package reservant_mobile.data.endpoints
+
+import io.ktor.resources.Resource
+
+@Resource("/ingredients")
+class Ingredients
--- app/src/main/java/reservant_mobile/data/models/dtos/IngredientDTO.kt
+++ app/src/main/java/reservant_mobile/data/models/dtos/IngredientDTO.kt
@@ -11,5 +11,13 @@ data class IngredientDTO (
     val unitOfMeasurement: UnitOfMeasurement? = null,
     val minimalAmount: Double? = null,
     val amountToOrder: Double? = null,
-    val amount: Double? = null
-)
+    val amount: Double? = null,
+    val menuItem: IngredientMenuItemDTO? = null
+){
+    @Serializable
+    data class IngredientMenuItemDTO(
+        val menuItemId: Int,
+        val amountUsed: Double
+    )
+
+}
