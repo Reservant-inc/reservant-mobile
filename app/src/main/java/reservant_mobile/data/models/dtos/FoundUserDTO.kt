cfd3fd0e81be0200685af72da9c0d74fdec83f9c
Maciej Mrozek <50219282+Mroxny@users.noreply.github.com>
2024-08-19 23:54:09 +0200

    Create FoundUserDTO.kt

---
+++ app/src/main/java/reservant_mobile/data/models/dtos/FoundUserDTO.kt
@@ -0,0 +1,20 @@
+package reservant_mobile.data.models.dtos
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class FoundUserDTO (
+    val userId: String? = null,
+    val firstName: String? = null,
+    val lastName: String? = null,
+    val photo: String? = null,
+    val friendStatus: FriendStatus? = null
+){
+    @Serializable
+    enum class FriendStatus{
+        Stranger,
+        OutgoingRequest,
+        IncomingRequest,
+        Friend
+    }
+}
