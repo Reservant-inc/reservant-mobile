7149fd235225898322de5c0ff400e1074ac00db0
Maciej Mrozek <50219282+Mroxny@users.noreply.github.com>
2024-09-10 21:27:10 +0200

    Implement routes

--- app/src/main/java/reservant_mobile/ui/activities/MainActivity.kt
+++ app/src/main/java/reservant_mobile/ui/activities/MainActivity.kt
@@ -7,6 +7,8 @@ import androidx.compose.runtime.Composable
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.launch
+import reservant_mobile.data.constants.Roles
+import reservant_mobile.data.services.UserService
 import reservant_mobile.ui.theme.AppTheme
 import reservant_mobile.ui.viewmodels.LoginViewModel
 
@@ -23,7 +25,12 @@ class MainActivity : ComponentActivity() {
 
         lifecycleScope.launch {
              startPoint = if(LoginViewModel().refreshToken()) {
-                 { HomeActivity() }
+                 if(Roles.RESTAURANT_EMPLOYEE in UserService.UserObject.roles) {
+                     { EmployeeHomeActivity() }
+                 }
+                 else{
+                     {HomeActivity()}
+                 }
              } else {
                  { LandingActivity() }
              }
