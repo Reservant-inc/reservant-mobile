01c9c27f84fe620c692825e39ffb24ec1c16217d
Maciej Mrozek <50219282+Mroxny@users.noreply.github.com>
2024-09-12 12:55:58 +0200

    refactor LocalBearerService

--- app/src/androidTest/java/reservant_mobile/services/ServiceTest.kt
+++ app/src/androidTest/java/reservant_mobile/services/ServiceTest.kt
@@ -5,12 +5,12 @@ import org.junit.After
 import org.junit.Before
 import reservant_mobile.data.models.dtos.LoginCredentialsDTO
 import reservant_mobile.data.services.IUserService
-import reservant_mobile.data.services.LocalBearerService
+import reservant_mobile.data.services.LocalDataService
 import reservant_mobile.data.services.UserService
 
 abstract class ServiceTest {
     private lateinit var existingToken: String
-    protected val localBearer = LocalBearerService()
+    protected val localBearer = LocalDataService()
     protected val userService: IUserService = UserService()
     protected var loginUser: LoginCredentialsDTO = LoginCredentialsDTO(
                                                     login = "JD",
@@ -19,14 +19,14 @@ abstract class ServiceTest {
                                                     )
     @Before
     fun getPreTestData() = runBlocking {
-        existingToken = localBearer.getBearerToken()
+        existingToken = localBearer.getData()
     }
 
     @After
     fun cleanPostTestData() = runBlocking{
         userService.logoutUser()
         if(existingToken.isNotEmpty())
-            localBearer.saveBearerToken(existingToken)
+            localBearer.saveData(existingToken)
     }
 
     suspend fun loginUser(){
