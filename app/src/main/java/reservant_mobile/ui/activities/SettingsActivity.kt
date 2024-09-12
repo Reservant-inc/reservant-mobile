38c88e8b7ee39f68a60c630100b9928179121740
Maciej Mrozek <50219282+Mroxny@users.noreply.github.com>
2024-09-12 22:43:00 +0200

    Update SettingsActivity.kt

--- app/src/main/java/reservant_mobile/ui/activities/SettingsActivity.kt
+++ app/src/main/java/reservant_mobile/ui/activities/SettingsActivity.kt
@@ -26,12 +26,16 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.example.reservant_mobile.R
 import kotlinx.coroutines.launch
+import reservant_mobile.data.constants.PrefsKeys
+import reservant_mobile.data.constants.Roles
+import reservant_mobile.data.services.LocalDataService
+import reservant_mobile.data.services.UserService
 import reservant_mobile.ui.components.IconWithHeader
 import reservant_mobile.ui.components.UnderlinedItem
 import reservant_mobile.ui.navigation.AuthRoutes
-import reservant_mobile.ui.navigation.UserRoutes
 import reservant_mobile.ui.navigation.MainRoutes
 import reservant_mobile.ui.navigation.RestaurantRoutes
+import reservant_mobile.ui.navigation.UserRoutes
 import reservant_mobile.ui.viewmodels.LoginViewModel
 
 @Composable
@@ -53,6 +57,7 @@ fun SettingsActivity(navController: NavController, themeChange: () -> Unit) {
 
             Spacer(modifier = Modifier.padding(top = 16.dp))
 
+            if(Roles.RESTAURANT_EMPLOYEE !in UserService.UserObject.roles )
             UnderlinedItem(
                 icon = Icons.Filled.Person,
                 text = stringResource(id = R.string.label_my_profile),
@@ -61,12 +66,14 @@ fun SettingsActivity(navController: NavController, themeChange: () -> Unit) {
                 }
             )
 
+            if(Roles.RESTAURANT_EMPLOYEE !in UserService.UserObject.roles )
             UnderlinedItem(
                 icon = Icons.Filled.AccountBalanceWallet,
                 text = stringResource(id = R.string.label_wallet),
                 onClick = { /* Navigate to Wallet */ }
             )
 
+            if(Roles.RESTAURANT_EMPLOYEE !in UserService.UserObject.roles )
             UnderlinedItem(
                 icon = Icons.Filled.ShoppingCart,
                 text = stringResource(id = R.string.label_my_orders),
@@ -85,6 +92,7 @@ fun SettingsActivity(navController: NavController, themeChange: () -> Unit) {
                 onClick = { /* Navigate to FAQ */ }
             )
 
+            if(Roles.RESTAURANT_EMPLOYEE !in UserService.UserObject.roles )
             UnderlinedItem(
                 icon = Icons.Filled.CardGiftcard,
                 text = stringResource(id = R.string.label_promo_codes),
@@ -97,6 +105,7 @@ fun SettingsActivity(navController: NavController, themeChange: () -> Unit) {
                 onClick = { /* Navigate to App Settings */ }
             )
 
+            if(Roles.RESTAURANT_EMPLOYEE !in UserService.UserObject.roles )
             UnderlinedItem(
                 icon = Icons.Filled.Delete,
                 text = stringResource(id = R.string.label_delete_account),
@@ -108,6 +117,12 @@ fun SettingsActivity(navController: NavController, themeChange: () -> Unit) {
                 text = stringResource(id = R.string.label_logout_action),
                 onClick = {
                     loginViewModel.viewModelScope.launch {
+                        if(Roles.RESTAURANT_EMPLOYEE in UserService.UserObject.roles ) {
+                            LocalDataService().saveData(
+                                key = PrefsKeys.EMPLOYEE_CURRENT_RESTAURANT,
+                                data = ""
+                            )
+                        }
                         loginViewModel.logout()
                         navController.navigate(AuthRoutes.Landing) {
                             popUpTo(0)
