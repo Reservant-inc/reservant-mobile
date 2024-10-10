03fb6a5e7a049462c1b9faaef5f2ac296a8e6e33
Aleksander Babij <aleksander.babij@gmail.com>
2024-10-10 21:48:04 +0200

    added circle shape image

--- app/src/main/java/reservant_mobile/ui/components/UserComponents.kt
+++ app/src/main/java/reservant_mobile/ui/components/UserComponents.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
@@ -13,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -42,6 +44,7 @@ fun UserCard(){
                     .padding(start = 8.dp ,end = 16.dp)
                     .size(50.dp)
                     .weight(0.2f)
+                    .clip(CircleShape),
             )
             Column(Modifier.weight(0.8f).align(Alignment.CenterVertically)) {
                 Text(
