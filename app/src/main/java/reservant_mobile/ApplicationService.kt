9c7909000e83bf7331145acd6a47af43686626e3
Maciej Mrozek <50219282+Mroxny@users.noreply.github.com>
2024-10-23 17:09:43 +0200

    Add ImageLoader config

--- app/src/main/java/reservant_mobile/ApplicationService.kt
+++ app/src/main/java/reservant_mobile/ApplicationService.kt
@@ -1,8 +1,15 @@
 package reservant_mobile
 
 import android.app.Application
+import coil3.ImageLoader
+import coil3.PlatformContext
+import coil3.SingletonImageLoader
+import coil3.annotation.ExperimentalCoilApi
+import coil3.disk.DiskCache
+import coil3.disk.directory
+import coil3.util.DebugLogger
 
-class ApplicationService: Application() {
+class ApplicationService: Application(), SingletonImageLoader.Factory {
 
     override fun onCreate() {
         super.onCreate()
@@ -12,5 +19,18 @@ class ApplicationService: Application() {
     companion object {
         lateinit var instance: ApplicationService
             private set
+    }
+
+    @OptIn(ExperimentalCoilApi::class)
+    override fun newImageLoader(context: PlatformContext): ImageLoader {
+        return ImageLoader.Builder(context)
+            .diskCache {
+                DiskCache.Builder()
+                    .directory(context.cacheDir.resolve("image_cache"))
+                    .maxSizePercent(0.1)
+                    .build()
+            }
+            .logger(DebugLogger())
+            .build()
     }
 }
