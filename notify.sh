04be5a7288baf33904c0d47fa94904d32c81b296
Maciej Mrozek <50219282+Mroxny@users.noreply.github.com>
2024-10-24 18:17:53 +0200

    Create notify.sh

---
+++ notify.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if [ -z "$DISCORD_WEBHOOK_URL" ]; then
+    echo "Error: DISCORD_WEBHOOK_URL environment variable is not set."
+    exit 1
+fi
+
+if [ -z "$ARTIFACT_ID" ]; then
+    echo "Error: ARTIFACT_ID environment variable is not set."
+    exit 1
+fi
+
+CURRENT_DATE=$(TZ=Etc/GMT-2 date +"%Y-%m-%d %H:%M:%S")
+ROLE_ID="1174437656688607353"
+MSG="<@&$ROLE_ID>\n## Mobile apk build success on __ $CURRENT_DATE __"
+ARTIFACT_URL="https://github.com/Reservant-inc/reservant-mobile/actions/runs/$ARTIFACT_ID"
+SUMMARY="Reservant mobile just released new build. The .apk file is available at (Artifacts section):\n$ARTIFACT_URL"
+
+curl -H "Content-Type: application/json" \
+  -X POST \
+  -d '{
+    "content": "'"${MSG}"'",
+    "embeds": [{
+      "title": "Frontend Test Summary",
+      "description": "'"${SUMMARY}"'",
+      "color": 15924992
+    }]
+  }' \
+  "$DISCORD_WEBHOOK_URL"
+
+echo "Summary sent to Discord webhook."
