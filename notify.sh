e5314b08722f8cf4286a6cb8d0b0a5bf19159244
Maciej Mrozek <50219282+Mroxny@users.noreply.github.com>
2024-10-24 14:49:52 +0200

    Update notify.sh

--- notify.sh
+++ notify.sh
@@ -13,7 +13,7 @@ fi
 CURRENT_DATE=$(TZ=Etc/GMT-2 date +"%Y-%m-%d %H:%M:%S")
 ROLE_ID="1174437656688607353"
 MSG="<@&$ROLE_ID>\n## Mobile apk build success on __ $CURRENT_DATE __"
-SUMMARY="Passed: **$PASSED_COUNT**\nErrors: **$ERRORS_COUNT**"
+SUMMARY="Reservant mobile just released new build. The artifact is available at:"
 ARTIFACT_URL="https://github.com/Reservant-inc/reservant-mobile/actions/runs/$ARTIFACT_ID"
 
 curl -H "Content-Type: application/json" \
