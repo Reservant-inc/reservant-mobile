d2893d08afaf95246d85793898167dab0536422a
Maciej Mrozek <50219282+Mroxny@users.noreply.github.com>
2024-10-24 14:32:43 +0200

    Update wf

--- notify.sh
+++ notify.sh
@@ -14,7 +14,7 @@ CURRENT_DATE=$(TZ=Etc/GMT-2 date +"%Y-%m-%d %H:%M:%S")
 ROLE_ID="1174437656688607353"
 MSG="<@&$ROLE_ID>\n## Mobile apk build success on __ $CURRENT_DATE __"
 SUMMARY="Passed: **$PASSED_COUNT**\nErrors: **$ERRORS_COUNT**"
-ARTIFACT_URL="https://github.com/Reservant-inc/reservant-mobile/actions/artifacts/$ARTIFACT_ID"
+ARTIFACT_URL="https://github.com/Reservant-inc/reservant-mobile/actions/runs/$ARTIFACT_ID"
 
 curl -H "Content-Type: application/json" \
   -X POST \
@@ -23,7 +23,7 @@ curl -H "Content-Type: application/json" \
     "embeds": [{
       "title": "Frontend Test Summary",
       "description": "'"${SUMMARY}"'",
-      "URL": ""
+      "URL": "'"${ARTIFACT_URL}"'"
       "color": 15924992
     }]
   }' \
