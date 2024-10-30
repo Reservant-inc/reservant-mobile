88acbced10ad0c5f22404a6b9a639b2d1bc78981
Dzwiedzi <108011192+Dzwiedzi@users.noreply.github.com>
2024-10-30 03:15:03 +0100

    refactor

--- app/src/main/java/reservant_mobile/ui/activities/EmployeeTablesActivity.kt
+++ app/src/main/java/reservant_mobile/ui/activities/EmployeeTablesActivity.kt
@@ -1,14 +1,15 @@
 package reservant_mobile.ui.activities
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.TableChart
 import androidx.compose.material3.Card
@@ -20,14 +21,17 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.reservant_mobile.R
 import reservant_mobile.data.models.dtos.TableDTO
 import reservant_mobile.ui.components.IconWithHeader
 import reservant_mobile.ui.viewmodels.TablesViewModel
+
 
 @Composable
 fun EmployeeTablesActivity(
@@ -46,18 +50,20 @@ fun EmployeeTablesActivity(
     Column(modifier = Modifier.fillMaxSize()) {
         IconWithHeader(
             icon = Icons.Rounded.TableChart,
-            text = "Restaurant Tables",
+            text = stringResource(id = R.string.restaurant_tables_header),
             showBackButton = true,
             onReturnClick = onReturnClick
         )
 
         Spacer(modifier = Modifier.height(16.dp))
 
-        LazyColumn(
+        LazyVerticalGrid(
+            columns = GridCells.Adaptive(minSize = 150.dp),
             modifier = Modifier
                 .fillMaxSize()
                 .padding(horizontal = 16.dp),
-            verticalArrangement = Arrangement.spacedBy(8.dp)
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
         ) {
             items(tables) { table ->
                 TableCard(table = table)
@@ -69,12 +75,10 @@ fun EmployeeTablesActivity(
 @Composable
 fun TableCard(table: TableDTO) {
     Card(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(vertical = 4.dp),
+        modifier = Modifier.padding(4.dp),
         elevation = CardDefaults.cardElevation(4.dp),
         colors = CardDefaults.cardColors(
-            containerColor = when (/**table.status**/"Occupied") {
+            containerColor = when (/**table.status**/"Available") { //TODO
                 "Available" -> Color(0xFFDFFFD6) // Light green for available
                 "Occupied" -> Color(0xFFFFD6D6) // Light red for occupied
                 else -> Color.LightGray // Default color for other statuses
@@ -82,18 +86,22 @@ fun TableCard(table: TableDTO) {
         )
     ) {
         Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp),
-            verticalArrangement = Arrangement.spacedBy(8.dp)
+            modifier = Modifier.padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(4.dp)
         ) {
             Text(
-                text = "Table ID: ${table.tableId}",
+                text = stringResource(id = R.string.table_label, table.tableId),
                 style = MaterialTheme.typography.titleMedium,
                 fontWeight = FontWeight.Bold
             )
             Text(
-                text = "Seats: ${table.capacity}",
+                text = stringResource(id = R.string.seats_label, table.capacity),
+                style = MaterialTheme.typography.bodyMedium
+            )
+            Text(
+                text = stringResource(
+                    id = if (/**table.status**/"Available" == "Available") R.string.available_label else R.string.occupied_label //TODO
+                ),
                 style = MaterialTheme.typography.bodyMedium
             )
         }
