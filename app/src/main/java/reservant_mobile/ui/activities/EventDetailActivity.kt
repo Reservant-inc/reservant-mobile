945166019613a7bcaa682448d1945056ca5605db
Michał Wiśniewski <s24845@pjwstk.edu.pl>
2024-11-03 03:40:31 +0100

    Update EventDetailActivity.kt

--- app/src/main/java/reservant_mobile/ui/activities/EventDetailActivity.kt
+++ app/src/main/java/reservant_mobile/ui/activities/EventDetailActivity.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.CalendarMonth
 import androidx.compose.material.icons.filled.Cancel
@@ -15,6 +16,8 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
@@ -61,8 +64,10 @@ fun EventDetailActivity(
                 Image(
                     painter = painterResource(id = R.drawable.restaurant_photo),
                     contentDescription = "Event Image",
+                    contentScale = ContentScale.Crop,
                     modifier = Modifier
-                        .fillMaxSize()
+                        .fillMaxWidth()
+                        .clip(shape = RoundedCornerShape(8.dp))
                 )
             }
 
