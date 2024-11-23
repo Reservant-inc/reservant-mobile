ffe8602f019277a72fd3c85edd438208fc8096d6
Maciej Mrozek <50219282+Mroxny@users.noreply.github.com>
2024-11-23 22:48:56 +0100

    Remove reporting user from restaurant service

--- app/src/androidTest/java/reservant_mobile/services/RestaurantServiceUnitTest.kt
+++ app/src/androidTest/java/reservant_mobile/services/RestaurantServiceUnitTest.kt
@@ -296,14 +296,4 @@ class RestaurantServiceUnitTest: ServiceTest() {
     fun get_tables_return_not_null()= runTest{
         assertThat(ser.getCurrentTables(restaurantId).value).isNotNull()
     }
-
-    @Test
-    fun report_user_return_not_null()= runTest{
-        assertThat(ser.reportCustomer(
-            description = "Test report desc",
-            reportedUserId = "e08ff043-f8d2-45d2-b89c-aec4eb6a1f29",
-            visitId = 1
-        ).value).isNotNull()
-    }
-
 }
