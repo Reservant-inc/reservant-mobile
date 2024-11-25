b936e662b1c47e07fcbb8e7dbce88d1fc11d48ea
Maciej Mrozek <50219282+Mroxny@users.noreply.github.com>
2024-11-25 14:35:52 +0100

    Update TableDTO

--- app/src/main/java/reservant_mobile/data/models/dtos/TableDTO.kt
+++ app/src/main/java/reservant_mobile/data/models/dtos/TableDTO.kt
@@ -1,10 +1,19 @@
 package reservant_mobile.data.models.dtos
 
+import com.example.reservant_mobile.R
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class TableDTO (
     val tableId: Int,
     val capacity: Int,
-    val visitId: Int? = null
-)
+    val visitId: Int? = null,
+    val status: TableStatus? = null
+){
+    @Serializable
+    enum class TableStatus(val stringId: Int){
+        Available(R.string.available_label),
+        Taken(R.string.taken_label),
+        VisitSoon(R.string.visit_soon_label)
+    }
+}
