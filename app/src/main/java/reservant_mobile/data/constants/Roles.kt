e5c58960bcbd8e035b1eda741194e2bce23cbde0
Aleksander Babij <aleksander.babij@gmail.com>
2024-12-10 19:26:18 +0100

    fixed BOK emp login

--- app/src/main/java/reservant_mobile/data/constants/Roles.kt
+++ app/src/main/java/reservant_mobile/data/constants/Roles.kt
@@ -7,5 +7,4 @@ data object Roles {
     const val RESTAURANT_BACKDOORS_EMPLOYEE = "RestaurantBackdoorsEmployee"
     const val RESTAURANT_HALL_EMPLOYEE = "RestaurantHallEmployee"
     const val CUSTOMER_SUPPORT_AGENT = "CustomerSupportAgent"
-    const val CUSTOMER_SUPPORT_MANAGER = "CustomerSupportManager"
 }
