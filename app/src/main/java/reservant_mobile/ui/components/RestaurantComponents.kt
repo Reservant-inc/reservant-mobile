07839417abc0f3e2b1a12277138fb06967b65546
Michał Wiśniewski <s24845@pjwstk.edu.pl>
2025-01-18 00:41:52 +0100

    Added CircularProgressIndicator in eventCard

--- app/src/main/java/reservant_mobile/ui/components/RestaurantComponents.kt
+++ app/src/main/java/reservant_mobile/ui/components/RestaurantComponents.kt
@@ -16,6 +16,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
@@ -236,14 +237,16 @@ fun EventCard(
                                 .clip(RoundedCornerShape(8.dp)),
                         )
                     }else{
-                        Image(
-                            painter = painterResource(R.drawable.restaurant_photo),
-                            contentScale = ContentScale.Crop,
-                            contentDescription = "Event Image",
+                        Box(
                             modifier = Modifier
                                 .fillMaxSize()
                                 .clip(RoundedCornerShape(8.dp)),
-                        )
+                            contentAlignment = Alignment.Center
+                        ) {
+                            CircularProgressIndicator(
+                                modifier = Modifier.size(48.dp)
+                            )
+                        }
                     }
 
                 }
